2f28853f0ce68672b1a0124edc2aa6894d3366e7
Phil Morrell <debian@emorrp1.name>
2018-05-04 20:08:17 +0100

    add initial debian packaging
    * dh_make --packagename play.it_2.7.5 --indep

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(play.it):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="play.it" command="/usr/bin/play.it"
---
+++ debian/play.it.default.ex
@@ -0,0 +1,10 @@
+# Defaults for play.it initscript
+# sourced by /etc/init.d/play.it
+# installed at /etc/default/play.it by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
