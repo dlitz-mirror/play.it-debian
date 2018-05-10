505983bd2d76500f084b9ff9a558165f09993988
Phil Morrell <debian@emorrp1.name>
2018-05-10 23:15:02 +0100

    remove unused template packaging

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(play.it):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="play.it" command="/usr/bin/play.it"
--- debian/play.it.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the play.it package
-#
-0 4	* * *	root	[ -x /usr/bin/play.it_maintenance ] && /usr/bin/play.it_maintenance
