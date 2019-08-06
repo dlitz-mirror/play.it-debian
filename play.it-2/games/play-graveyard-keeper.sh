8febea09bd70a010c310f9c1c37a6e283e1b4c57
vv221 <vv221@dotslashplay.it>
2019-08-06 18:35:24 +0200

    Garveyard Keeper — Fix dependencies list

--- play.it-2/games/play-graveyard-keeper.sh
+++ play.it-2/games/play-graveyard-keeper.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20210513.1
+script_version=20210513.2
 
 # Set game-specific variables
 
@@ -71,7 +71,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="${PKG_DATA_ID} glibc libstdc++"
+PKG_BIN32_DEPS="${PKG_DATA_ID} glibc libstdc++ gtk2 glx libudev1"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
