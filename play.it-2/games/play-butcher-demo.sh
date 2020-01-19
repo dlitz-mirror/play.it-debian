606eee191487fd9c7e9bf21641159c5887bbf169
vv221 <vv221@dotslashplay.it>
2020-01-19 14:45:03 +0100

    Butcher Demo — Syntax update
    * Update authors list
    * Drop call to icons_linking_postinst
    * Minor syntax tweaks

--- play.it-2/games/play-butcher-demo.sh
+++ play.it-2/games/play-butcher-demo.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200131.1
+script_version=20200131.2
 
 # Set game-specific variables
 
@@ -111,17 +111,19 @@ extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
+# Get game icon
+
+PKG='PKG_DATA'
+icons_get_from_package 'APP_MAIN'
+
 # Write launchers
 
 PKG='PKG_BIN'
-launcher_write 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
-PKG='PKG_DATA'
-icons_linking_postinst 'APP_MAIN'
-write_metadata 'PKG_DATA'
-write_metadata 'PKG_BIN'
+write_metadata
 build_pkg
 
 # Clean up
