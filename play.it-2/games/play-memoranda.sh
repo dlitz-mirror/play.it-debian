435f90ddae64605029009349c33ad952203d4987
vv221 <vv221@dotslashplay.it>
2020-02-04 21:13:47 +0100

    Memoranda — Syntax update
    * Update authors list
    * Update script to 2.11 syntax

--- play.it-2/games/play-memoranda.sh
+++ play.it-2/games/play-memoranda.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
 # Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2016-2020, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,11 +31,11 @@ set -o errexit
 
 ###
 # Memoranda
-# build native Linux packages from the original installers
-# send your bug reports to mopi@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20180303.3
+script_version=20200204.7
 
 # Set game-specific variables
 
@@ -43,32 +44,25 @@ SCRIPT_DEPS='convert'
 GAME_ID='memoranda'
 GAME_NAME='Memoranda'
 
-ARCHIVES_LIST='ARCHIVE_GOG'
-
 ARCHIVE_GOG='gog_memoranda_2.2.0.3.sh'
 ARCHIVE_GOG_URL='https://www.gog.com/game/memoranda'
 ARCHIVE_GOG_MD5='9671ebb592d4b4a028fd80f76e96c1a1'
 ARCHIVE_GOG_SIZE='800000'
 ARCHIVE_GOG_VERSION='1.0-gog2.2.0.3'
-ARCHIVE_GOG_TYPE='mojosetup'
 
-ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
-
-ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC_DATA_FILES='./*'
+ARCHIVE_OPTIONAL_LIBSSL32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_OPTIONAL_LIBSSL32_URL='https://www.dotslashplay.it/ressources/libssl/'
+ARCHIVE_OPTIONAL_LIBSSL32_MD5='9443cad4a640b2512920495eaf7582c4'
 
 ARCHIVE_GAME_BIN_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN_FILES='./runner'
+ARCHIVE_GAME_BIN_FILES='runner'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='./assets'
+ARCHIVE_GAME_DATA_FILES='assets'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='runner'
-APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
 APP_MAIN_ICON='assets/icon.png'
-APP_MAIN_ICON_RES='1024'
 
 PACKAGES_LIST='PKG_BIN PKG_DATA'
 
@@ -81,72 +75,75 @@ PKG_BIN_DEPS_ARCH='lib32-openssl-1.0'
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
-# Use libSSL 1.0.0 32-bit archive
+# Use libSSL 1.0.0 32-bit archive (Debian packages only)
 
-if [ "$OPTION_PACKAGE" != 'arch' ]; then
+if [ "$OPTION_PACKAGE" = 'deb' ]; then
 	ARCHIVE_MAIN="$ARCHIVE"
-	set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_32'
+	archive_set 'ARCHIVE_LIBSSL32' 'ARCHIVE_OPTIONAL_LIBSSL32'
 	ARCHIVE="$ARCHIVE_MAIN"
 fi
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
-
+prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
-# Change icon resolution
+# Get game icon
+
+PKG='PKG_DATA'
+icons_get_from_package 'APP_MAIN'
+
+# Build 512×512 icon from the 1024×1024 provided one
 
 PATH_ICON="${PATH_ICON_BASE}/512x512/apps"
 mkdir --parents "${PKG_DATA_PATH}${PATH_ICON}"
 convert "${PKG_DATA_PATH}${PATH_GAME}/${APP_MAIN_ICON}" -resize 512 "${PKG_DATA_PATH}${PATH_ICON}/${GAME_ID}.png"
 
-# Include libSSL into the game directory
+# Include libSSL 1.0.0 (Debian packages only)
 
-if [ "$ARCHIVE_LIBSSL" ]; then
-	(
-		ARCHIVE='ARCHIVE_LIBSSL'
-		extract_data_from "$ARCHIVE_LIBSSL"
-	)
-	dir='libs'
-	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/$dir"
-	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN_PATH}${PATH_GAME}/$dir"
-	APP_MAIN_LIBS="$dir"
+if [ "$ARCHIVE_LIBSSL32" ]; then
+	ARCHIVE='ARCHIVE_LIBSSL32' \
+		extract_data_from "$ARCHIVE_LIBSSL32"
+	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/${APP_MAIN_LIBS:=libs}"
+	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN_PATH}${PATH_GAME}/$APP_MAIN_LIBS"
 	rm --recursive "$PLAYIT_WORKDIR/gamedata"
 fi
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
-postinst_icons_linking 'APP_MAIN'
-write_metadata 'PKG_DATA'
-write_metadata 'PKG_BIN'
+write_metadata
 build_pkg
 
 # Clean up
