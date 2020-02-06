894f83137de779365adbe4c76a7fbc2cbf75fa9d
vv221 <vv221@dotslashplay.it>
2020-02-06 17:29:28 +0100

    Evoland 2 — Fix performance issues
    With WINE 5.0, performances are poor if libGL.so.1 is not available

--- play.it-2/games/play-evoland-2.sh
+++ play.it-2/games/play-evoland-2.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200206.1
+script_version=20200206.2
 
 # Set game-specific variables
 
@@ -66,7 +66,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID wine"
+PKG_BIN_DEPS="$PKG_DATA_ID wine glx"
 
 # Load common functions
 
