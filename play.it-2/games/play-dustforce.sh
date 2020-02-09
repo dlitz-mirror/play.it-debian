664504951c75e2976cc5147252126e3dd7d7eb85
vv221 <vv221@dotslashplay.it>
2020-02-09 22:29:39 +0100

    Dustforce DX — Fix dependencies list

--- play.it-2/games/play-dustforce.sh
+++ play.it-2/games/play-dustforce.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200209.1
+script_version=20200219.1
 
 # Set game-specific variables
 
@@ -71,10 +71,16 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ sdl2 alsa glx openal"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx libxrandr alsa libudev1"
+PKG_BIN32_DEPS_ARCH='lib32-libidn11'
+PKG_BIN32_DEPS_DEB='libidn11'
+PKG_BIN32_DEPS_GENTOO='net-dns/libidn[abi_x86_32]'
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
+PKG_BIN64_DEPS_ARCH='libidn11'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_GENTOO='net-dns/libidn'
 
 # Load common functions
 
