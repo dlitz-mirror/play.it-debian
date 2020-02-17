9beb98e4e8d5dbeac9c26778e35790594be33d15
macaron <23-macaron@users.noreply.forge.dotslashplay.it>
2020-02-17 11:18:18 +0100

    Little Big Adventure — Add launcher and app for setup.exe (language selection, among others)

--- play.it-2/games/play-little-big-adventure-1.sh
+++ play.it-2/games/play-little-big-adventure-1.sh
@@ -4,6 +4,7 @@ set -o errexit
 ###
 # Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
 # Copyright (c) 2018-2020, Dominique Derrier
+# Copyright (c) 2020, macaron
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -35,7 +36,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200314.1
+script_version=20200329.2
 
 # Set game-specific variables
 
@@ -71,6 +72,13 @@ DATA_FILES='*.LBA'
 APP_MAIN_TYPE='dosbox'
 APP_MAIN_EXE='relent.exe'
 APP_MAIN_ICON='app/goggame-1207658971.ico'
+
+APP_SETUP_TYPE='dosbox'
+APP_SETUP_EXE='setup.exe'
+APP_SETUP_ID="${GAME_ID}_setup"
+APP_SETUP_NAME="$GAME_NAME - Setup"
+APP_SETUP_CAT='Settings'
+APP_SETUP_ICON="$APP_MAIN_ICON"
 
 PACKAGES_LIST='PKG_MAIN'
 
@@ -113,12 +121,12 @@ prepare_package_layout
 
 # Extract icons
 
-icons_get_from_workdir 'APP_MAIN'
+icons_get_from_workdir 'APP_MAIN' 'APP_SETUP'
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-launchers_write 'APP_MAIN'
+launchers_write 'APP_MAIN' 'APP_SETUP'
 
 # Build package
 
