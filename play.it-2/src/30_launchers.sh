171f6a191481f937626459adee38d680a1ce3e30
macaron <23-macaron@users.noreply.forge.dotslashplay.it>
2020-03-07 13:20:48 +0100

    Add helper function for converting filenames in launcher scripts

--- play.it-2/src/30_launchers.sh
+++ play.it-2/src/30_launchers.sh
@@ -358,7 +358,31 @@ launcher_write_script_prefix_functions() {
 	    display_message "$@"
 	}
 
-	# Set prefix-related functions
+	# Set userdir- and prefix-related functions
+
+	# convert the name of specified user files and directories to upper case
+	# note that only the last component of each matching path is converted
+	# USAGE: userdir_toupper_files $userdir $list
+	userdir_toupper_files() {
+	    local userdir
+	    local list
+	    userdir="$1"
+	    list="$2"
+	    (
+	        cd "$userdir"
+	        for file in $list; do
+	            [ -e "$file" ] || continue
+	            newfile=$(dirname "$file")/$(basename "$file" | tr '[:lower:]' '[:upper:]')
+	            if [ ! -e "$newfile" ]; then
+	                mv "$file" "$newfile"
+	            else
+	                display_warning \
+	                    "en:Cannot overwrite '$userdir/${newfile#./}' with '$userdir/$file'" \
+	                    "fr:Impossible d'écraser '$userdir/${newfile#./}' par '$userdir/$file'"
+	            fi
+	        done
+	    )
+	}
 
 	init_prefix_dirs() {
 	    # shellcheck disable=SC2039
