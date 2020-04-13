2eb43e768836addb034de2893dd7cb613d7385b0
BetaRays <BetaRays+playit@protonmail.com>
2020-04-13 13:22:34 +0200

    World of Goo — Syntax update
    * Update script to 2.11 syntax
    * Stop using find for *.binltl* files
    * Do not rely on dynamically generated variables for icons handling

--- play.it-2/games/play-world-of-goo.sh
+++ play.it-2/games/play-world-of-goo.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
 # Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2018-2020, BetaRays
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -31,14 +32,12 @@ set -o errexit
 ###
 # World of Goo
 # build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20180802.1
+script_version=20200413.14
 
 # Set game-specific variables
-
-SCRIPT_DEPS='find'
 
 GAME_ID='world-of-goo'
 GAME_NAME='World of Goo'
@@ -50,31 +49,38 @@ ARCHIVE_GOG_SIZE='82000'
 ARCHIVE_GOG_VERSION='1.41-gog2.0.0.3'
 
 ARCHIVE_DOC0_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC0_DATA_FILES='./*'
+ARCHIVE_DOC0_DATA_FILES='*'
 
 ARCHIVE_DOC1_DATA_PATH='data/noarch/game'
-ARCHIVE_DOC1_DATA_FILES='./*.html ./*.txt'
+ARCHIVE_DOC1_DATA_FILES='*.html *.txt'
 
 ARCHIVE_GAME_BIN32_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN32_FILES='./WorldOfGoo.bin32 ./libs32'
+ARCHIVE_GAME_BIN32_FILES='WorldOfGoo.bin32 libs32 res/*/*.binltl res/*/*/*.binltl'
 
 ARCHIVE_GAME_BIN64_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN64_FILES='./WorldOfGoo.bin64 ./libs64'
+ARCHIVE_GAME_BIN64_FILES='WorldOfGoo.bin64 libs64 res/*/*.binltl64 res/*/*/*.binltl64'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='./icons ./properties ./res'
+ARCHIVE_GAME_DATA_FILES='icons properties res'
 
 CONFIG_FILES='./properties/config.txt'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE_BIN32='WorldOfGoo.bin32'
 APP_MAIN_EXE_BIN64='WorldOfGoo.bin64'
-unset APP_MAIN_ICONS_LIST
-for res in 16 22 32 48 64 128; do
-	APP_MAIN_ICONS_LIST="$APP_MAIN_ICONS_LIST APP_MAIN_ICON_${res}"
-	export APP_MAIN_ICON_${res}="icons/${res}x${res}.png"
-	export APP_MAIN_ICON_${res}_RES="$res"
-done
+APP_MAIN_ICONS_LIST='
+APP_MAIN_ICON16
+APP_MAIN_ICON22
+APP_MAIN_ICON32
+APP_MAIN_ICON48
+APP_MAIN_ICON64
+APP_MAIN_ICON128'
+APP_MAIN_ICON16='icons/16x16.png'
+APP_MAIN_ICON22='icons/22x22.png'
+APP_MAIN_ICON32='icons/32x32.png'
+APP_MAIN_ICON48='icons/48x48.png'
+APP_MAIN_ICON64='icons/64x64.png'
+APP_MAIN_ICON128='icons/128x128.png'
 
 PACKAGES_LIST='PKG_BIN32 PKG_BIN64 PKG_DATA'
 
@@ -85,87 +91,66 @@ PKG_BIN32_ARCH='32'
 PKG_BIN32_DEPS="$PKG_DATA_ID glu vorbis sdl1.2"
 PKG_BIN32_DEPS_ARCH='lib32-sdl_mixer'
 PKG_BIN32_DEPS_DEB='libsdl-mixer1.2'
+PKG_BIN32_DEPS_GENTOO='media-libs/sdl-mixer[vorbis,abi_x86_32]'
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 PKG_BIN64_DEPS_ARCH='sdl_mixer'
 PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_GENTOO='media-libs/sdl-mixer[vorbis]'
 
 # Load common functions
 
-target_version='2.7'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
-PKG='PKG_BIN32'
-organize_data 'GAME_BIN32' "$PATH_GAME"
-(
-	cd "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_GAME_BIN32_PATH"
-	find res -name '*.binltl' | while read -r file; do
-		cp --parents "$file" "${PKG_BIN32_PATH}${PATH_GAME}"
-		rm "$file"
-	done
-)
-
-PKG='PKG_BIN64'
-organize_data 'GAME_BIN64' "$PATH_GAME"
-(
-	cd "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_GAME_BIN64_PATH"
-	find res -name '*.binltl64' | while read -r file; do
-		cp --parents "$file" "${PKG_BIN64_PATH}${PATH_GAME}"
-		rm "$file"
-	done
-)
+# Get game icons
 
 PKG='PKG_DATA'
-find "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_GAME_DATA_PATH/res" -type d -empty -delete
-organize_data 'DOC0_DATA' "$PATH_DOC"
-organize_data 'DOC1_DATA' "$PATH_DOC"
-organize_data 'GAME_DATA' "$PATH_GAME"
-
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+icons_get_from_package 'APP_MAIN'
+# Include SVG icon
+PATH_ICON="$PATH_ICON_BASE/scalable/apps"
+mkdir --parents "${PKG_DATA_PATH}/${PATH_ICON}"
+cp "${PKG_DATA_PATH}/${PATH_GAME}/icons/scalable.svg" "${PKG_DATA_PATH}/${PATH_ICON}/${GAME_ID}.svg"
 
 # Write launchers
 
 for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
-	write_launcher 'APP_MAIN'
+	launchers_write 'APP_MAIN'
 done
 
 # Build package
 
-PATH_ICON="$PATH_ICON_BASE/scalable/apps"
-cat > "$postinst" << EOF
-if [ ! -f "$PATH_ICON/${GAME_ID}.svg" ]; then
-	mkdir --parents "$PATH_ICON"
-	ln --symbolic "$PATH_GAME/icons/scalable.svg" "$PATH_ICON/${GAME_ID}.svg"
-fi
-EOF
-cat > "$prerm" << EOF
-if [ -f "$PATH_ICON/${GAME_ID}.svg" ]; then
-	rm "$PATH_ICON/${GAME_ID}.svg"
-	rmdir --ignore-fail-on-non-empty --parents "$PATH_ICON"
-fi
-EOF
-postinst_icons_linking 'APP_MAIN'
-write_metadata 'PKG_DATA'
-write_metadata 'PKG_BIN32' 'PKG_BIN64'
+write_metadata
 build_pkg
 
 # Clean up
