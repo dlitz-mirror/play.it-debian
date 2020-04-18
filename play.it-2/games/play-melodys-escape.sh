e85638b113cdcc5614f1452e89c6bd28035fa1c8
vv221 <vv221@dotslashplay.it>
2020-04-19 00:21:26 +0200

    Melodyʼs Escape — Syntax update

--- play.it-2/games/play-melodys-escape.sh
+++ play.it-2/games/play-melodys-escape.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
 # Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2016-2020, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -29,20 +30,17 @@ set -o errexit
 ###
 
 ###
-# Melody’s Escape
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# Melodyʼs Escape
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200918.1
+script_version=20201003.1
 
 # Set game-specific variables
 
 GAME_ID='melodys-escape'
-# shellcheck disable=SC1112
-GAME_NAME='Melody’s Escape'
-
-ARCHIVES_LIST='ARCHIVE_HUMBLE'
+GAME_NAME='Melodyʼs Escape'
 
 ARCHIVE_HUMBLE='Melodys_Escape_Linux.zip'
 ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/melodys-escape'
@@ -50,22 +48,24 @@ ARCHIVE_HUMBLE_MD5='4d463482418c2d9917c56df3bbde6eea'
 ARCHIVE_HUMBLE_SIZE='60000'
 ARCHIVE_HUMBLE_VERSION='1.0-humble160601'
 
-ARCHIVE_ICONS_PACK='melodys-escape_icons.tar.gz'
-ARCHIVE_ICONS_PACK_MD5='656fce13728d399e557fd72c3a6bc244'
+ARCHIVE_OPTIONAL_ICONS='melodys-escape_icons.tar.gz'
+ARCHIVE_OPTIONAL_ICONS_MD5='656fce13728d399e557fd72c3a6bc244'
+ARCHIVE_OPTIONAL_ICONS_URL='https://downloads.dotslashplay.it/resources/melodys-escape/'
 
-ARCHIVE_DOC_PATH="Melody's Escape"
-ARCHIVE_DOC_FILES='./Licenses ./README.txt'
+ARCHIVE_DOC_DATA_PATH="Melody's Escape"
+ARCHIVE_DOC_DATA_FILES='Licenses README.txt'
 
 ARCHIVE_GAME_BIN_PATH="Melody's Escape"
-ARCHIVE_GAME_BIN_FILES='./MelodysEscape.bin.x86 ./lib ./*.dll ./FNA.dll.config ./*.so ./MelodysEscape.exe'
+ARCHIVE_GAME_BIN_FILES='MelodysEscape.bin.x86 lib *.dll FNA.dll.config *.so MelodysEscape.exe'
 
 ARCHIVE_GAME_DATA_PATH="Melody's Escape"
-ARCHIVE_GAME_DATA_FILES='./BassPlugins ./BundledMusic ./Calibration ./Content ./Mods ./mono'
+ARCHIVE_GAME_DATA_FILES='BassPlugins BundledMusic Calibration Content Mods mono'
 
 ARCHIVE_ICONS_PATH='.'
-ARCHIVE_ICONS_FILES='./16x16 ./32x32 ./48x48 ./64x64 ./128x128 ./256x256'
+ARCHIVE_ICONS_FILES='16x16 32x32 48x48 64x64 128x128 256x256'
 
 APP_MAIN_TYPE='native'
+APP_MAIN_LIBS='lib'
 APP_MAIN_EXE='MelodysEscape.bin.x86'
 
 PACKAGES_LIST='PKG_DATA PKG_BIN'
@@ -74,12 +74,11 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6"
-PKG_BIN_DEPS_ARCH="$PKG_DATA_ID lib32-glibc"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++"
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
@@ -102,43 +101,41 @@ if [ -z "$PLAYIT_LIB2" ]; then
 	printf 'libplayit2.sh not found.\n'
 	exit 1
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
-# Try to load icons archive
+# Load optional archives
 
 ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'ARCHIVE_ICONS' 'ARCHIVE_ICONS_PACK'
+set_archive 'ARCHIVE_ICONS' 'ARCHIVE_OPTIONAL_ICONS'
 ARCHIVE="$ARCHIVE_MAIN"
+if [ -n "$ARCHIVE_LIB64" ]; then
+	PACKAGES_LIST="$PACKAGES_LIST_LIB64"
+	set_temp_directories $PACKAGES_LIST
+fi
 
-# Extract game data
+# Include optional icons pack
 
-extract_data_from "$SOURCE_ARCHIVE"
-if [ "$ARCHIVE_ICONS" ]; then
+if [ -n "$ARCHIVE_ICONS" ]; then
+	PKG='PKG_DATA'
 	(
 		ARCHIVE='ARCHIVE_ICONS'
 		extract_data_from "$ARCHIVE_ICONS"
 	)
-fi
-
-PKG='PKG_BIN'
-organize_data 'GAME_BIN' "$PATH_GAME"
-
-PKG='PKG_DATA'
-organize_data 'DOC'       "$PATH_DOC"
-organize_data 'GAME_DATA' "$PATH_GAME"
-
-PKG='PKG_DATA'
-if [ "$ARCHIVE_ICONS" ]; then
 	organize_data 'ICONS' "$PATH_ICON_BASE"
+	rm --recursive "$PLAYIT_WORKDIR/gamedata"
 fi
 
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
