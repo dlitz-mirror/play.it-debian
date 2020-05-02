8f1c99a4453d2f86ccc7f3833afabb0eb46f7eed
vv221 <vv221@dotslashplay.it>
2020-05-02 21:34:21 +0200

    Dust: An Elysian Tale — Syntax update

--- play.it-2/games/play-dust-an-elysian-tale.sh
+++ play.it-2/games/play-dust-an-elysian-tale.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
@@ -30,102 +30,99 @@ set -o errexit
 
 ###
 # Dust: An Elysian Tale
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20200502.1
 
 # Set game-specific variables
 
 GAME_ID='dust-an-elysian-tale'
 GAME_NAME='Dust: An Elysian Tale'
 
-ARCHIVES_LIST='ARCHIVE_HUMBLE'
+ARCHIVES_LIST='
+ARCHIVE_HUMBLE_0
+'
 
-ARCHIVE_HUMBLE='dustaet_05042016-bin'
-ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/dust-an-elysian-tail'
-ARCHIVE_HUMBLE_MD5='6844c82f233b47417620be0bef8b140c'
-ARCHIVE_HUMBLE_SIZE='1500000'
-ARCHIVE_HUMBLE_VERSION='1.04-humble160504'
-ARCHIVE_HUMBLE_TYPE='mojosetup'
+ARCHIVE_HUMBLE_0='dustaet_05042016-bin'
+ARCHIVE_HUMBLE_0_URL='https://www.humblebundle.com/store/dust-an-elysian-tail'
+ARCHIVE_HUMBLE_0_MD5='6844c82f233b47417620be0bef8b140c'
+ARCHIVE_HUMBLE_0_SIZE='1500000'
+ARCHIVE_HUMBLE_0_VERSION='1.04-humble160504'
+ARCHIVE_HUMBLE_0_TYPE='mojosetup'
 
-ARCHIVE_DOC_PATH='data'
-ARCHIVE_DOC_FILES='./Linux.README'
+ARCHIVE_DOC_DATA_PATH='data'
+ARCHIVE_DOC_DATA_FILES='Linux.README'
 
 ARCHIVE_GAME_BIN32_PATH='data'
-ARCHIVE_GAME_BIN32_FILES='./*.x86 ./lib'
+ARCHIVE_GAME_BIN32_FILES='DustAET.bin.x86 lib'
 
 ARCHIVE_GAME_BIN64_PATH='data'
-ARCHIVE_GAME_BIN64_FILES='./*.x86_64 ./lib64'
+ARCHIVE_GAME_BIN64_FILES='DustAET.bin.x86_64 lib64'
 
 ARCHIVE_GAME_DATA_PATH='data'
-ARCHIVE_GAME_DATA_FILES='./*'
+ARCHIVE_GAME_DATA_FILES='Content data de es fr it ja DustAET.exe Dust?An?Elysian?Tail.bmp monoconfig *.dll *.dll.config'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_EXE_BIN32='./DustAET.bin.x86'
-APP_MAIN_EXE_BIN64='./DustAET.bin.x86_64'
+APP_MAIN_EXE_BIN32='DustAET.bin.x86'
+APP_MAIN_EXE_BIN64='DustAET.bin.x86_64'
 APP_MAIN_ICON='Dust An Elysian Tail.bmp'
-APP_MAIN_ICON_RES='64'
 
-PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
+PACKAGES_LIST='PKG_BIN32 PKG_BIN64 PKG_DATA'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libogg0, libopenal1, libsdl2-2.0-0, libtheora0, libvorbisfile3, libvorbis0a"
-PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-libogg lib32-openal lib32-sdl2 lib32-libtheora lib32-libvorbis"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ vorbis sdl2 theora"
 
 PKG_BIN64_ARCH='64'
-PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
-PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID libogg openal sdl2 libtheora libvorbis"
+PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
 # Load common functions
 
-target_version='2.0'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
-PKG='PKG_BIN32'
-organize_data 'GAME_BIN32' "$PATH_GAME"
-
-PKG='PKG_BIN64'
-organize_data 'GAME_BIN64' "$PATH_GAME"
+# Get game icon
 
 PKG='PKG_DATA'
-organize_data 'DOC'       "$PATH_DOC"
-organize_data 'GAME_DATA' "$PATH_GAME"
-
-res="$APP_MAIN_ICON_RES"
-PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
-extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON"
-mkdir --parents "${PKG_DATA_PATH}${PATH_ICON}"
-mv "$PLAYIT_WORKDIR/icons/${APP_MAIN_ICON%.bmp}.png" "${PKG_DATA_PATH}${PATH_ICON}/$GAME_ID.png"
-
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+icons_get_from_package 'APP_MAIN'
 
 # Write launchers
 
 for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
-	write_launcher 'APP_MAIN'
+	launchers_write 'APP_MAIN'
 done
 
 # Build package
@@ -139,11 +136,7 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-printf '\n'
-printf '32-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN32'
-printf '64-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN64'
+print_instructions
 
 exit 0
 
