71b3bab7453e526abafd2c1ed713be98d9701eff
vv221 <vv221@dotslashplay.it>
2020-05-15 01:58:49 +0200

    Stellaris — Use a new naming scheme for archives

--- play.it-2/games/play-stellaris-distant-stars-story-pack.sh
+++ play.it-2/games/play-stellaris-distant-stars-story-pack.sh
@@ -34,79 +34,92 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200427.1
+script_version=20200515.1
 
 # Set game-specific variables
 
 GAME_ID='stellaris'
 GAME_NAME='Stellaris - Distant Stars Story Pack'
 
-ARCHIVE_GOG='stellaris_distant_stars_story_pack_2_6_3_2_37617.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/stellaris_distant_stars_story_pack'
-ARCHIVE_GOG_MD5='bac93cb96d38a6c5dea49bd2e42eaa87'
-ARCHIVE_GOG_SIZE='22000'
-ARCHIVE_GOG_VERSION='2.6.3.2-gog37617'
-ARCHIVE_GOG_TYPE='mojosetup_unzip'
+ARCHIVES_LIST='
+ARCHIVE_GOG_10
+ARCHIVE_GOG_9
+ARCHIVE_GOG_8
+ARCHIVE_GOG_7
+ARCHIVE_GOG_6
+ARCHIVE_GOG_5
+ARCHIVE_GOG_4
+ARCHIVE_GOG_3
+ARCHIVE_GOG_2
+ARCHIVE_GOG_1
+ARCHIVE_GOG_0'
 
-ARCHIVE_GOG_OLD9='stellaris_distant_stars_story_pack_2_6_2_37285.sh'
-ARCHIVE_GOG_OLD9_MD5='69437917f67cdde8125a64de3627f439'
-ARCHIVE_GOG_OLD9_SIZE='22000'
-ARCHIVE_GOG_OLD9_VERSION='2.6.2-gog37285'
-ARCHIVE_GOG_OLD9_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_10='stellaris_distant_stars_story_pack_2_6_3_2_37617.sh'
+ARCHIVE_GOG_10_URL='https://www.gog.com/game/stellaris_distant_stars_story_pack'
+ARCHIVE_GOG_10_MD5='bac93cb96d38a6c5dea49bd2e42eaa87'
+ARCHIVE_GOG_10_SIZE='22000'
+ARCHIVE_GOG_10_VERSION='2.6.3.2-gog37617'
+ARCHIVE_GOG_10_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD8='stellaris_distant_stars_story_pack_2_6_1_1_36932.sh'
-ARCHIVE_GOG_OLD8_MD5='7cce4073c357a05f8be6e6444b1c7ae8'
-ARCHIVE_GOG_OLD8_SIZE='22000'
-ARCHIVE_GOG_OLD8_VERSION='2.6.1.1-gog36932'
-ARCHIVE_GOG_OLD8_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_9='stellaris_distant_stars_story_pack_2_6_2_37285.sh'
+ARCHIVE_GOG_9_MD5='69437917f67cdde8125a64de3627f439'
+ARCHIVE_GOG_9_SIZE='22000'
+ARCHIVE_GOG_9_VERSION='2.6.2-gog37285'
+ARCHIVE_GOG_9_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD7='stellaris_distant_stars_story_pack_2_6_0_4_36778.sh'
-ARCHIVE_GOG_OLD7_MD5='19a959edefc55a03a0ed7e410fb1d4ec'
-ARCHIVE_GOG_OLD7_SIZE='22000'
-ARCHIVE_GOG_OLD7_VERSION='2.6.0.4-gog36778'
-ARCHIVE_GOG_OLD7_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_8='stellaris_distant_stars_story_pack_2_6_1_1_36932.sh'
+ARCHIVE_GOG_8_MD5='7cce4073c357a05f8be6e6444b1c7ae8'
+ARCHIVE_GOG_8_SIZE='22000'
+ARCHIVE_GOG_8_VERSION='2.6.1.1-gog36932'
+ARCHIVE_GOG_8_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD6='stellaris_distant_stars_story_pack_2_5_1_33517.sh'
-ARCHIVE_GOG_OLD6_MD5='bb38b6691d39123d1503ea517b4896ea'
-ARCHIVE_GOG_OLD6_SIZE='22000'
-ARCHIVE_GOG_OLD6_VERSION='2.5.1-gog33517'
-ARCHIVE_GOG_OLD6_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_7='stellaris_distant_stars_story_pack_2_6_0_4_36778.sh'
+ARCHIVE_GOG_7_MD5='19a959edefc55a03a0ed7e410fb1d4ec'
+ARCHIVE_GOG_7_SIZE='22000'
+ARCHIVE_GOG_7_VERSION='2.6.0.4-gog36778'
+ARCHIVE_GOG_7_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD5='stellaris_distant_stars_story_pack_2_5_0_5_33395.sh'
-ARCHIVE_GOG_OLD5_MD5='f7e4d861c265ee1aab8c505df415390a'
-ARCHIVE_GOG_OLD5_SIZE='22000'
-ARCHIVE_GOG_OLD5_VERSION='2.5.0.5-gog33395'
-ARCHIVE_GOG_OLD5_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_6='stellaris_distant_stars_story_pack_2_5_1_33517.sh'
+ARCHIVE_GOG_6_MD5='bb38b6691d39123d1503ea517b4896ea'
+ARCHIVE_GOG_6_SIZE='22000'
+ARCHIVE_GOG_6_VERSION='2.5.1-gog33517'
+ARCHIVE_GOG_6_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD4='stellaris_distant_stars_story_pack_2_4_1_1_33112.sh'
-ARCHIVE_GOG_OLD4_MD5='13a3c2f25da4e39be55b6d0b6cd40826'
-ARCHIVE_GOG_OLD4_SIZE='22000'
-ARCHIVE_GOG_OLD4_VERSION='2.4.1.1-gog33112'
-ARCHIVE_GOG_OLD4_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_5='stellaris_distant_stars_story_pack_2_5_0_5_33395.sh'
+ARCHIVE_GOG_5_MD5='f7e4d861c265ee1aab8c505df415390a'
+ARCHIVE_GOG_5_SIZE='22000'
+ARCHIVE_GOG_5_VERSION='2.5.0.5-gog33395'
+ARCHIVE_GOG_5_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD3='stellaris_distant_stars_story_pack_2_4_1_33088.sh'
-ARCHIVE_GOG_OLD3_MD5='17716ff7cf3fb83a418064a2505ad9d6'
-ARCHIVE_GOG_OLD3_SIZE='22000'
-ARCHIVE_GOG_OLD3_VERSION='2.4.1-gog33088'
-ARCHIVE_GOG_OLD3_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_4='stellaris_distant_stars_story_pack_2_4_1_1_33112.sh'
+ARCHIVE_GOG_4_MD5='13a3c2f25da4e39be55b6d0b6cd40826'
+ARCHIVE_GOG_4_SIZE='22000'
+ARCHIVE_GOG_4_VERSION='2.4.1.1-gog33112'
+ARCHIVE_GOG_4_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD2='stellaris_distant_stars_story_pack_2_4_0_7_33057.sh'
-ARCHIVE_GOG_OLD2_MD5='1b6272a16391842d48906845435d03c5'
-ARCHIVE_GOG_OLD2_SIZE='22000'
-ARCHIVE_GOG_OLD2_VERSION='2.4.0.7-gog33057'
-ARCHIVE_GOG_OLD2_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_3='stellaris_distant_stars_story_pack_2_4_1_33088.sh'
+ARCHIVE_GOG_3_MD5='17716ff7cf3fb83a418064a2505ad9d6'
+ARCHIVE_GOG_3_SIZE='22000'
+ARCHIVE_GOG_3_VERSION='2.4.1-gog33088'
+ARCHIVE_GOG_3_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD1='stellaris_distant_stars_story_pack_2_3_3_1_30901.sh'
-ARCHIVE_GOG_OLD1_MD5='2cd17f30fd465528c4c45427b3ffc45a'
-ARCHIVE_GOG_OLD1_SIZE='22000'
-ARCHIVE_GOG_OLD1_VERSION='2.3.3.1-gog30901'
-ARCHIVE_GOG_OLD1_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_2='stellaris_distant_stars_story_pack_2_4_0_7_33057.sh'
+ARCHIVE_GOG_2_MD5='1b6272a16391842d48906845435d03c5'
+ARCHIVE_GOG_2_SIZE='22000'
+ARCHIVE_GOG_2_VERSION='2.4.0.7-gog33057'
+ARCHIVE_GOG_2_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD0='stellaris_distant_stars_story_pack_2_3_3_30733.sh'
-ARCHIVE_GOG_OLD0_MD5='c66aa0592a5f188196cfd3ab8a75bd15'
-ARCHIVE_GOG_OLD0_SIZE='22000'
-ARCHIVE_GOG_OLD0_VERSION='2.3.3-gog30733'
-ARCHIVE_GOG_OLD0_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_1='stellaris_distant_stars_story_pack_2_3_3_1_30901.sh'
+ARCHIVE_GOG_1_MD5='2cd17f30fd465528c4c45427b3ffc45a'
+ARCHIVE_GOG_1_SIZE='22000'
+ARCHIVE_GOG_1_VERSION='2.3.3.1-gog30901'
+ARCHIVE_GOG_1_TYPE='mojosetup_unzip'
+
+ARCHIVE_GOG_0='stellaris_distant_stars_story_pack_2_3_3_30733.sh'
+ARCHIVE_GOG_0_MD5='c66aa0592a5f188196cfd3ab8a75bd15'
+ARCHIVE_GOG_0_SIZE='22000'
+ARCHIVE_GOG_0_VERSION='2.3.3-gog30733'
+ARCHIVE_GOG_0_TYPE='mojosetup_unzip'
 
 ARCHIVE_GAME_MAIN_PATH='data/noarch/game'
 ARCHIVE_GAME_MAIN_FILES='dlc/dlc019_distant_stars'
