94e51f29d07e31bde4c7d7cdc2e89880007bebdf
vv221 <vv221@dotslashplay.it>
2020-06-08 18:35:52 +0200

    Beatbuddy: Tale of the Guardians — Update incomplete dependencies list

--- play.it-2/games/play-beatbuddy.sh
+++ play.it-2/games/play-beatbuddy.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200608.1
+script_version=20200608.2
 
 # Set game-specific variables
 
@@ -72,7 +72,10 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTIOn='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID glu xcursor alsa"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glu glx xcursor gtk2 alsa"
+PKG_BIN_DEPS_ARCH='lib32-libx11'
+PKG_BIN_DEPS_DEB='libx11-6'
+PKG_BIN_DEPS_GENTOO='x11-libs/libX11[abi_x86_32]'
 
 # Load common functions
 
