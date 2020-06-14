3e196f199ee0a22f7fdce5b207e4eabb7a55c5ee
vv221 <vv221@dotslashplay.it>
2020-06-14 17:41:59 +0200

    Star Wars: Knights of the Old Republic Ⅱ — New archives

--- play.it-2/games/play-star-wars-knights-of-the-old-republic-2.sh
+++ play.it-2/games/play-star-wars-knights-of-the-old-republic-2.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200614.1
+script_version=20200614.2
 
 # Set game-specific variables
 
@@ -42,44 +42,74 @@ GAME_ID='star-wars-knights-of-the-old-republic-2'
 GAME_NAME='Star Wars: Knights of the Old Republic Ⅱ - The Sith Lords'
 
 ARCHIVES_LIST='
-ARCHIVE_GOG_EN_0
-ARCHIVE_GOG_FR_0
+ARCHIVE_GOG_EN_1
+ARCHIVE_GOG_FR_1
+ARCHIVE_GOG_RAR_EN_0
+ARCHIVE_GOG_RAR_FR_0
 '
 
-ARCHIVE_GOG_EN_0='setup_sw_kotor2_2.0.0.3.exe'
-ARCHIVE_GOG_EN_0_URL='https://www.gog.com/game/star_wars_knights_of_the_old_republic_ii_the_sith_lords'
-ARCHIVE_GOG_EN_0_MD5='0163b31f8763b77f567f5646d2586b61'
-ARCHIVE_GOG_EN_0_TYPE='rar'
-ARCHIVE_GOG_EN_0_VERSION='1.0b-gog2.0.0.3'
-ARCHIVE_GOG_EN_0_SIZE='4700000'
-ARCHIVE_GOG_EN_0_PART1='setup_sw_kotor2_2.0.0.3-1.bin'
-ARCHIVE_GOG_EN_0_PART1_MD5='bbedad0d349a653a1502f2b9f4c207fc'
-ARCHIVE_GOG_EN_0_PART1_TYPE='rar'
+ARCHIVE_GOG_EN_1='setup_star_wars_-_knights_of_the_old_republic_ii_1.0b_(29869).exe'
+ARCHIVE_GOG_EN_1_URL='https://www.gog.com/game/star_wars_knights_of_the_old_republic_ii_the_sith_lords'
+ARCHIVE_GOG_EN_1_MD5='7f7a2e14e5ebadf14c0cdbb1ee807521'
+ARCHIVE_GOG_EN_1_TYPE='innosetup'
+ARCHIVE_GOG_EN_1_VERSION='1.0b-gog29869'
+ARCHIVE_GOG_EN_1_SIZE='4700000'
+ARCHIVE_GOG_EN_1_PART1='setup_star_wars_-_knights_of_the_old_republic_ii_1.0b_(29869)-1.bin'
+ARCHIVE_GOG_EN_1_PART1_MD5='8092cf5da5fa165f88d67e172c610c5e'
+ARCHIVE_GOG_EN_1_PART1_TYPE='innosetup'
 
-ARCHIVE_GOG_FR_0='setup_sw_kotor2_french_2.0.0.3.exe'
-ARCHIVE_GOG_FR_0_URL='https://www.gog.com/game/star_wars_knights_of_the_old_republic_ii_the_sith_lords'
-ARCHIVE_GOG_FR_0_MD5='81eae2db19c61a25111f2e6e5960a751'
-ARCHIVE_GOG_FR_0_TYPE='rar'
-ARCHIVE_GOG_FR_0_VERSION='1.0b-gog2.0.0.3'
-ARCHIVE_GOG_FR_0_SIZE='4600000'
-ARCHIVE_GOG_FR_0_PART1='setup_sw_kotor2_french_2.0.0.3-1.bin'
-ARCHIVE_GOG_FR_0_PART1_MD5='27a4f0ba820bc66f53aa5117684917cf'
-ARCHIVE_GOG_FR_0_PART1_TYPE='rar'
+ARCHIVE_GOG_FR_1='setup_star_wars_-_knights_of_the_old_republic_ii_1.0b_(french)_(29869).exe'
+ARCHIVE_GOG_FR_1_URL='https://www.gog.com/game/star_wars_knights_of_the_old_republic_ii_the_sith_lords'
+ARCHIVE_GOG_FR_1_MD5='a16a80f377111ec4152e5d1b196f64f5'
+ARCHIVE_GOG_FR_1_TYPE='innosetup'
+ARCHIVE_GOG_FR_1_VERSION='1.0b-gog29869'
+ARCHIVE_GOG_FR_1_SIZE='4600000'
+ARCHIVE_GOG_FR_1_PART1='setup_star_wars_-_knights_of_the_old_republic_ii_1.0b_(french)_(29869)-1.bin'
+ARCHIVE_GOG_FR_1_PART1_MD5='e68c85d7f0ad6212c9841276526aa5d3'
+ARCHIVE_GOG_FR_1_PART1_TYPE='innosetup'
 
-ARCHIVE_DOC_DATA_PATH='game'
+ARCHIVE_GOG_RAR_EN_0='setup_sw_kotor2_2.0.0.3.exe'
+ARCHIVE_GOG_RAR_EN_0_MD5='0163b31f8763b77f567f5646d2586b61'
+ARCHIVE_GOG_RAR_EN_0_TYPE='rar'
+ARCHIVE_GOG_RAR_EN_0_VERSION='1.0b-gog2.0.0.3'
+ARCHIVE_GOG_RAR_EN_0_SIZE='4700000'
+ARCHIVE_GOG_RAR_EN_0_PART1='setup_sw_kotor2_2.0.0.3-1.bin'
+ARCHIVE_GOG_RAR_EN_0_PART1_MD5='bbedad0d349a653a1502f2b9f4c207fc'
+ARCHIVE_GOG_RAR_EN_0_PART1_TYPE='rar'
+
+ARCHIVE_GOG_RAR_FR_0='setup_sw_kotor2_french_2.0.0.3.exe'
+ARCHIVE_GOG_RAR_FR_0_MD5='81eae2db19c61a25111f2e6e5960a751'
+ARCHIVE_GOG_RAR_FR_0_TYPE='rar'
+ARCHIVE_GOG_RAR_FR_0_VERSION='1.0b-gog2.0.0.3'
+ARCHIVE_GOG_RAR_FR_0_SIZE='4600000'
+ARCHIVE_GOG_RAR_FR_0_PART1='setup_sw_kotor2_french_2.0.0.3-1.bin'
+ARCHIVE_GOG_RAR_FR_0_PART1_MD5='27a4f0ba820bc66f53aa5117684917cf'
+ARCHIVE_GOG_RAR_FR_0_PART1_TYPE='rar'
+
+ARCHIVE_DOC_DATA_PATH='.'
 ARCHIVE_DOC_DATA_FILES='*.pdf *.txt'
+# Keep compatibility with old archives
+ARCHIVE_DOC_DATA_PATH_GOG_RAR='game'
 
-ARCHIVE_GAME1_BIN_PATH='game'
+ARCHIVE_GAME1_BIN_PATH='.'
 ARCHIVE_GAME1_BIN_FILES='*.exe binkw32.dll mss32.dll patchw32.dll miles utils'
+# Keep compatibility with old archives
+ARCHIVE_GAME1_BIN_PATH_GOG_RAR='game'
 
-ARCHIVE_GAME2_BIN_PATH='support/app'
+ARCHIVE_GAME2_BIN_PATH='__support/app'
 ARCHIVE_GAME2_BIN_FILES='*.ini'
+# Keep compatibility with old archives
+ARCHIVE_GAME2_BIN_PATH_GOG_RAR='support/app'
 
-ARCHIVE_GAME_L10N_PATH='game'
+ARCHIVE_GAME_L10N_PATH='.'
 ARCHIVE_GAME_L10N_FILES='dialog.tlk override/*.2da override/*.gui override/*.tpc override/*.wav lips streamsounds/a_* streamsounds/n_* streamsounds/p_* streamvoice movies/kre* movies/permov01.bik movies/scn* movies/trailer.bik'
+# Keep compatibility with old archives
+ARCHIVE_GAME_L10N_PATH_GOG_RAR='game'
 
-ARCHIVE_GAME_DATA_PATH='game'
+ARCHIVE_GAME_DATA_PATH='.'
 ARCHIVE_GAME_DATA_FILES='chitin.key override/*.mdl modules streammusic streamsounds/al_* streamsounds/amb_* streamsounds/as_* streamounds/avo_* streamsounds/c_* streamsounds/dr_* streamsounds/echo_* streamsounds/evt_* streamsounds/mgs_* streamsounds/mus_* texturepacks data movies/credits.bik movies/dan* movies/hyp* movies/kho* movies/kor* movies/leclogo.bik movies/legal.bik movies/mal* movies/nar* movies/obsidianent.bik movies/ond* movies/permov02.bik movies/permov03.bik movies/permov04.bik movies/permov05.bik movies/permov06.bik movies/permov07.bik movies/tel*'
+# Keep compatibility with old archives
+ARCHIVE_GAME_DATA_PATH_GOG_RAR='game'
 
 CONFIG_FILES='./*.ini'
 DATA_DIRS='./override ./saves'
@@ -97,12 +127,23 @@ APP_CONFIG_ICON='swconfig.exe'
 
 PACKAGES_LIST='PKG_L10N PKG_DATA PKG_BIN'
 
+# Localization package - common properties
 PKG_L10N_ID="${GAME_ID}-l10n"
-PKG_L10N_ID_GOG_EN="${PKG_L10N_ID}-en"
-PKG_L10N_ID_GOG_FR="${PKG_L10N_ID}-fr"
 PKG_L10N_PROVIDE="$PKG_L10N_ID"
+
+# Localization package - English
+PKG_L10N_ID_GOG_EN="${PKG_L10N_ID}-en"
 PKG_L10N_DESCRIPTION_GOG_EN='English localization'
+# Keep compatibility with old archives
+PKG_L10N_ID_GOG_RAR_EN="$PKG_L10N_ID_GOG_EN"
+PKG_L10N_DESCRIPTION_GOG_RAR_EN="$PKG_L10N_DESCRIPTION_GOG_EN"
+
+# Localization package - French
+PKG_L10N_ID_GOG_FR="${PKG_L10N_ID}-fr"
 PKG_L10N_DESCRIPTION_GOG_FR='French localization'
+# Keep compatibility with old archives
+PKG_L10N_ID_GOG_RAR_FR="$PKG_L10N_ID_GOG_FR"
+PKG_L10N_DESCRIPTION_GOG_RAR_FR="$PKG_L10N_DESCRIPTION_GOG_FR"
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
@@ -141,8 +182,15 @@ fi
 
 # Extract game data
 
-extract_data_from "$SOURCE_ARCHIVE_PART1"
-tolower "$PLAYIT_WORKDIR/gamedata"
+case "$ARCHIVE" in
+	('ARCHIVE_GOG_RAR_'*)
+		extract_data_from "$SOURCE_ARCHIVE_PART1"
+		tolower "$PLAYIT_WORKDIR/gamedata"
+	;;
+	(*)
+		extract_data_from "$SOURCE_ARCHIVE"
+	;;
+esac
 prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
