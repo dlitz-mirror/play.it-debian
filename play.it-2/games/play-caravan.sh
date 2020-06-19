c3cc7ce390f3f8885e4d3fac1802355177227cf4
vv221 <vv221@dotslashplay.it>
2020-06-19 02:24:49 +0200

    Caravan — Syntax update
    * Update script to 2.11 syntax
    * Update authors list
    * Use new archive naming scheme
    * Drop obsolete icons_linking_postinst

--- play.it-2/games/play-caravan.sh
+++ play.it-2/games/play-caravan.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
 # Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2016-2020, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,34 +31,36 @@ set -o errexit
 
 ###
 # Caravan
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20200619.1
 
 # Set game-specific variables
 
 GAME_ID='caravan'
 GAME_NAME='Caravan'
 
-ARCHIVES_LIST='ARCHIVE_HUMBLE'
+ARCHIVES_LIST='
+ARCHIVE_HUMBLE_0
+'
 
-ARCHIVE_HUMBLE='caravan_linux_v1.1.17513.zip'
-ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/caravan'
-ARCHIVE_HUMBLE_MD5='286f762190d74e27c353b5616b9e6dee'
-ARCHIVE_HUMBLE_SIZE='600000'
-ARCHIVE_HUMBLE_VERSION='1.1.17513-humble1'
-ARCHIVE_HUMBLE_TYPE='zip_unclean'
+ARCHIVE_HUMBLE_0='caravan_linux_v1.1.17513.zip'
+ARCHIVE_HUMBLE_0_URL='https://www.humblebundle.com/store/caravan'
+ARCHIVE_HUMBLE_0_MD5='286f762190d74e27c353b5616b9e6dee'
+ARCHIVE_HUMBLE_0_SIZE='600000'
+ARCHIVE_HUMBLE_0_VERSION='1.1.17513-humble1'
+ARCHIVE_HUMBLE_0_TYPE='zip_unclean'
 
 ARCHIVE_GAME_BIN32_PATH='Caravan'
-ARCHIVE_GAME_BIN32_FILES='./*.x86 ./*_Data/*/x86'
+ARCHIVE_GAME_BIN32_FILES='Caravan.x86 Caravan_Data/Mono/x86 Caravan_Data/Plugins/x86'
 
 ARCHIVE_GAME_BIN64_PATH='Caravan'
-ARCHIVE_GAME_BIN64_FILES='./*.x86_64 ./*_Data/*/x86_64'
+ARCHIVE_GAME_BIN64_FILES='Caravan.x86_64 Caravan_Data/Mono/x86_64 Caravan_Data/Plugins/x86_64'
 
 ARCHIVE_GAME_DATA_PATH='Caravan'
-ARCHIVE_GAME_DATA_FILES='./*_Data/*.assets ./*_Data/mainData ./*_Data/Mono/etc ./*_Data/Managed ./*_Data/Resources ./*_Data/Plugins'
+ARCHIVE_GAME_DATA_FILES='Caravan_Data'
 
 DATA_DIRS='./logs'
 
@@ -66,9 +69,7 @@ APP_MAIN_EXE_BIN32='Caravan.x86'
 APP_MAIN_EXE_BIN64='Caravan.x86_64'
 # shellcheck disable=SC2016
 APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
-APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
-APP_MAIN_ICON='*_Data/Resources/UnityPlayer.png'
-APP_MAIN_ICON_RES='128'
+APP_MAIN_ICON='Caravan_Data/Resources/UnityPlayer.png'
 
 PACKAGES_LIST='PKG_BIN32 PKG_BIN64 PKG_DATA'
 
@@ -83,49 +84,60 @@ PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-
-ARCHIVE_HUMBLE_TYPE='tar.gz'
-extract_data_from "$PLAYIT_WORKDIR/gamedata/Caravan_1.1.17513_Full_DEB_Multi_Daedalic_ESD.tar.gz"
-rm "$PLAYIT_WORKDIR/gamedata/Caravan_1.1.17513_Full_DEB_Multi_Daedalic_ESD"*
+(
+	ARCHIVE='ARCHIVE_INNER'
+	ARCHIVE_INNER="$PLAYIT_WORKDIR/gamedata/Caravan_1.1.17513_Full_DEB_Multi_Daedalic_ESD.tar.gz"
+	ARCHIVE_INNER_TYPE='tar.gz'
+	extract_data_from "$ARCHIVE_INNER"
+	rm "$ARCHIVE_INNER"
+)
 set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
-
-for PKG in $PACKAGES_LIST; do
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
-
+prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Get game icon
+
+PKG='PKG_DATA'
+icons_get_from_package 'APP_MAIN'
 
 # Write launchers
 
 for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
-	write_launcher 'APP_MAIN'
+	launchers_write 'APP_MAIN'
 done
 
 # Build package
 
-postinst_icons_linking 'APP_MAIN'
-write_metadata 'PKG_DATA'
-write_metadata 'PKG_BIN32' 'PKG_BIN64'
+write_metadata
 build_pkg
 
 # Clean up
@@ -134,10 +146,6 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-printf '\n'
-printf '32-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN32'
-printf '64-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN64'
+print_instructions
 
 exit 0
