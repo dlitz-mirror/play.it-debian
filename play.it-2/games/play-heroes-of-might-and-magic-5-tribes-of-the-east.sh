6632af57f4d53adba483aa77fbb436e558ae6842
vv221 <vv221@dotslashplay.it>
2020-07-16 15:42:39 +0200

    Heroes of Might and Magic Ⅴ - Tribes of the East — Syntax update

--- play.it-2/games/play-heroes-of-might-and-magic-5-tribes-of-the-east.sh
+++ play.it-2/games/play-heroes-of-might-and-magic-5-tribes-of-the-east.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
@@ -29,141 +29,161 @@ set -o errexit
 ###
 
 ###
-# Heroes of Might and Magic V - Tribes of the East
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# Heroes of Might and Magic Ⅴ - Tribes of the East
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20200716.1
 
 # Set game-specific variables
 
 GAME_ID='heroes-of-might-and-magic-5-tribes-of-the-east'
-GAME_NAME='Heroes of Might and Magic V - Tribes of the East'
+GAME_NAME='Heroes of Might and Magic Ⅴ - Tribes of the East'
 
-ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
+ARCHIVES_LIST='
+ARCHIVE_GOG_EN_0
+ARCHIVE_GOG_FR_0'
 
-ARCHIVE_GOG_EN='setup_homm5_tote_2.1.0.24.bin'
-ARCHIVE_GOG_EN_URL='https://www.gog.com/game/heroes_of_might_and_magic_5_bundle'
-ARCHIVE_GOG_EN_MD5='48a783c1f6d3e15a0439fc58d85c5b28'
-ARCHIVE_GOG_EN_TYPE='rar'
-ARCHIVE_GOG_EN_SIZE='2300000'
-ARCHIVE_GOG_EN_VERSION='3.1-gog2.1.0.24'
+ARCHIVE_GOG_EN_0='setup_homm5_tote_2.1.0.24.exe'
+ARCHIVE_GOG_EN_0_MD5='80db7a846cb8a7052506db814cb2185e'
+ARCHIVE_GOG_EN_0_TYPE='rar'
+ARCHIVE_GOG_EN_0_URL='https://www.gog.com/game/heroes_of_might_and_magic_5_bundle'
+ARCHIVE_GOG_EN_0_SIZE='2300000'
+ARCHIVE_GOG_EN_0_VERSION='3.1-gog2.1.0.24'
+ARCHIVE_GOG_EN_0_PART1='setup_homm5_tote_2.1.0.24.bin'
+ARCHIVE_GOG_EN_0_PART1_MD5='48a783c1f6d3e15a0439fc58d85c5b28'
+ARCHIVE_GOG_EN_0_PART1_TYPE='rar'
 
-ARCHIVE_GOG_FR='setup_homm5_tote_french_2.1.0.24-1.bin'
-ARCHIVE_GOG_FR_URL='https://www.gog.com/game/heroes_of_might_and_magic_5_bundle'
-ARCHIVE_GOG_FR_MD5='bfb583edb64c548cf60f074e4abc2043'
-ARCHIVE_GOG_FR_TYPE='rar'
-ARCHIVE_GOG_FR_SIZE='2300000'
-ARCHIVE_GOG_FR_VERSION='3.1-gog2.1.0.24'
+ARCHIVE_GOG_FR_0='setup_homm5_tote_french_2.1.0.24.exe'
+ARCHIVE_GOG_FR_0_MD5='67f1c9b8ddeae0707729ebfb8ec05e51'
+ARCHIVE_GOG_FR_0_TYPE='rar'
+ARCHIVE_GOG_FR_0_URL='https://www.gog.com/game/heroes_of_might_and_magic_5_bundle'
+ARCHIVE_GOG_FR_0_SIZE='2300000'
+ARCHIVE_GOG_FR_0_VERSION='3.1-gog2.1.0.24'
+ARCHIVE_GOG_FR_0_PART1='setup_homm5_tote_french_2.1.0.24-1.bin'
+ARCHIVE_GOG_FR_0_PART1_MD5='bfb583edb64c548cf60f074e4abc2043'
+ARCHIVE_GOG_FR_0_PART1_TYPE='rar'
 
 ARCHIVE_DOC_L10N_PATH='game'
-ARCHIVE_DOC_L10N_FILES='./*.pdf ./*.txt ./editor?documentation ./fandocuments/*.pdf ./fandocuments/*.txt'
+ARCHIVE_DOC_L10N_FILES='*.pdf *.txt editor?documentation fandocuments/*.pdf fandocuments/*.txt'
 
 ARCHIVE_GAME_BIN_PATH='game'
-ARCHIVE_GAME_BIN_FILES='./bindm ./bin/*.exe ./bin/*.ini ./bin/dbghelp.dll ./bin/fmod.dll ./bin/granny2.dll ./bin/libcurl.dll ./bin/mfc71.dll ./bin/mfc71enu.dll ./bin/msvcp71.dll ./bin/msvcr71.dll ./bin/ubistats.dll ./bin/um.dll ./bin/zlib1.dll ./bin/zlibwapi.dll'
+ARCHIVE_GAME_BIN_FILES='bin bindm'
 
 ARCHIVE_GAME_L10N_PATH='game'
-ARCHIVE_GAME_L10N_FILES='./fandocuments/*.exe ./data/a2p1-texts.pak ./data/sound.pak ./data/texts.pak'
+ARCHIVE_GAME_L10N_FILES='fandocuments/*.exe data/a2p1-texts.pak data/sound.pak data/texts.pak'
 
 ARCHIVE_GAME_DATA_PATH='game'
-ARCHIVE_GAME_DATA_FILES='./*.bmp ./customcontentdm ./editor ./hwcursors ./music ./profiles ./video ./data/a2p1-data.pak ./data/data.pak ./data/soundsfx.pak'
+ARCHIVE_GAME_DATA_FILES='*.bmp customcontentdm editor hwcursors music profiles video data/a2p1-data.pak data/data.pak data/soundsfx.pak'
 
 APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='bin/h5_game.exe'
 APP_MAIN_ICON='bin/h5_game.exe'
-APP_MAIN_ICON_RES='16 32 48 64 256'
+APP_MAIN_PRERUN='# Run the game binary from its parent directory
+cd "$(dirname "$APP_EXE")"
+APP_EXE=$(basename "$APP_EXE")'
 
-APP_EDIT_ID="${GAME_ID}_edit"
+APP_EDIT_ID="${GAME_ID}_map-editor"
+APP_EDIT_NAME="$GAME_NAME - Map Editor"
 APP_EDIT_TYPE='wine'
 APP_EDIT_EXE='bin/h5_mapeditor.exe'
 APP_EDIT_ICON='bin/h5_mapeditor.exe'
 APP_EDIT_ICON_ID='128'
-APP_EDIT_ICON_RES='32'
-APP_EDIT_NAME="$GAME_NAME - Map Editor"
+APP_EDIT_PRERUN="$APP_MAIN_PRERUN"
 
-APP_DM_ID="${GAME_ID}_dm"
+APP_DM_ID="${GAME_ID}_dark-messiah"
+APP_DM_NAME="$GAME_NAME - Dark Messiah"
 APP_DM_TYPE='wine'
 APP_DM_EXE='bindm/h5_game.exe'
 APP_DM_ICON='bindm/h5_game.exe'
 APP_DM_ICON_ID='101'
-APP_DM_ICON_RES='16 32 48 64 256'
-APP_DM_NAME="$GAME_NAME - Dark Messiah"
+APP_DM_PRERUN="$APP_MAIN_PRERUN"
 
-APP_SKILLS_ID="${GAME_ID}_skills"
+APP_SKILLS_ID="${GAME_ID}_skill-wheel"
+APP_SKILLS_NAME="$GAME_NAME - SkillWheel"
 APP_SKILLS_TYPE='wine'
 APP_SKILLS_EXE='fandocuments/skillwheel.exe'
 APP_SKILLS_ICON='fandocuments/skillwheel.exe'
 APP_SKILLS_ICON_ID='200'
-APP_SKILLS_ICON_RES='16 32 48'
-APP_SKILLS_NAME="$GAME_NAME - SkillWheel"
+APP_SKILLS_PRERUN="$APP_MAIN_PRERUN"
 
 PACKAGES_LIST='PKG_L10N PKG_DATA PKG_BIN'
 
+# Localization — common properties
 PKG_L10N_ID="${GAME_ID}-l10n"
-PKG_L10N_ID_GOG_EN="${GAME_ID}-l10n-en"
-PKG_L10N_ID_GOG_FR="${GAME_ID}-l10n-fr"
 PKG_L10N_PROVIDE="$PKG_L10N_ID"
+# Localization — English
+PKG_L10N_ID_GOG_EN="${PKG_L10N_ID}-en"
 PKG_L10N_DESCRIPTION_GOG_EN='English localization'
+# Localization — French
+PKG_L10N_ID_GOG_FR="${PKG_L10N_ID}-fr"
 PKG_L10N_DESCRIPTION_GOG_FR='French localization'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, $PKG_L10N_ID, wine32-development | wine32 | wine-bin | wine-i386 | wine-staging-i386, wine:amd64 | wine"
-PKG_BIN_DEPS_ARCH="$PKG_DATA_ID $PKG_L10N_ID wine"
+PKG_BIN_DEPS="$PKG_DATA_ID $PKG_L10N_ID wine glx"
 
 # Load common functions
 
-target_version='2.1'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
-extract_data_from "$SOURCE_ARCHIVE"
+extract_data_from "$SOURCE_ARCHIVE_PART1"
 tolower "$PLAYIT_WORKDIR/gamedata"
+prepare_package_layout
 
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
+# Get icons
+
+###
+# TODO
+# icons_move_to fails if the destination is not empty
+# see https://forge.dotslashplay.it/play.it/scripts/-/issues/233
+###
 
 PKG='PKG_BIN'
-extract_and_sort_icons_from 'APP_MAIN' 'APP_EDIT' 'APP_DM'
-move_icons_to 'PKG_DATA'
+icons_get_from_package 'APP_MAIN' 'APP_EDIT' 'APP_DM'
+icons_move_to 'PKG_L10N'
 
 PKG='PKG_L10N'
-extract_and_sort_icons_from 'APP_SKILLS'
-move_icons_to 'PKG_DATA'
+icons_get_from_package 'APP_SKILLS'
+icons_move_to 'PKG_DATA'
+
+# Clean up temporary files
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN' 'APP_EDIT' 'APP_DM' 'APP_SKILLS'
-(
-	cd "${PKG_BIN_PATH}${PATH_BIN}"
-	# shellcheck disable=SC2016
-	sed --in-place 's|cd "$PATH_PREFIX"|cd "$PATH_PREFIX/${APP_EXE%/*}"|'                     "$GAME_ID" "$APP_EDIT_ID" "$APP_DM_ID"
-	# shellcheck disable=SC2016
-	sed --in-place 's|wine "$APP_EXE" $APP_OPTIONS $@|wine "${APP_EXE##*/}" $APP_OPTIONS $@|' "$GAME_ID" "$APP_EDIT_ID" "$APP_DM_ID"
-)
+launchers_write 'APP_MAIN' 'APP_EDIT' 'APP_DM' 'APP_SKILLS'
 
 # Build package
 
