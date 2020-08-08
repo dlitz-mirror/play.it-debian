1533968b4c0dc045ac70079f639924af03d38e3c
vv221 <vv221@dotslashplay.it>
2020-08-08 16:20:36 +0200

    Bump library revision

--- play.it-2/src/00_header.sh
+++ play.it-2/src/00_header.sh
@@ -40,7 +40,7 @@
 # send your bug reports to contact@dotslashplay.it
 ###
 
-library_version=2.12.0~dev
+library_version=2.12.0
 # shellcheck disable=SC2034
-library_revision=20200803.1
+library_revision=20200808.1
 
