d5f94537efb775c0723ed49295295c68856a550a
vv221 <vv221@dotslashplay.it>
2020-08-08 10:47:35 +0200

    Shadow Tactics: Blades of the Shogun — Add missing dependencies
    When PulseAudio is running, ALSA plugins are required to get working
    audio.

--- play.it-2/games/play-shadow-tactics.sh
+++ play.it-2/games/play-shadow-tactics.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200808.1
+script_version=20200808.2
 
 # Set game-specific variables
 
@@ -87,7 +87,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_LIGHTING_ID $PKG_DATA_ID glibc libstdc++ glx libudev1 xcursor libxrandr"
+PKG_BIN_DEPS="$PKG_LIGHTING_ID $PKG_DATA_ID glibc libstdc++ glx libudev1 xcursor libxrandr alsa"
 
 # Load common functions
 
