cf9cf72e72e8b1a4704dbf5a55352c8ed592a5d5
vv221 <vv221@dotslashplay.it>
2020-08-12 14:06:18 +0200

    Crypt of the NecroDancer — Syntax update

--- play.it-2/games/play-crypt-of-the-necrodancer.sh
+++ play.it-2/games/play-crypt-of-the-necrodancer.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
 # Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2016-2020, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,70 +31,67 @@ set -o errexit
 
 ###
 # Crypt of the Necrodancer
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200918.1
+script_version=20200930.1
 
 # Set game-specific variables
 
 GAME_ID='crypt-of-the-necrodancer'
 GAME_NAME='Crypt of the NecroDancer'
 
-ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_GOG_OLD ARCHIVE_GOG_OLDER ARCHIVE_GOG_OLDEST'
+ARCHIVES_LIST='
+ARCHIVE_GOG_3
+ARCHIVE_GOG_2
+ARCHIVE_GOG_1
+ARCHIVE_GOG_0'
 
-ARCHIVE_GOG='crypt_of_the_necrodancer_en_1_29_14917.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/crypt_of_the_necrodancer'
-ARCHIVE_GOG_MD5='70d3e29a2a48901d02541d8b1c6326ba'
-ARCHIVE_GOG_SIZE='1600000'
-ARCHIVE_GOG_VERSION='1.29-gog14917'
-ARCHIVE_GOG_TYPE='mojosetup'
+ARCHIVE_GOG_3='crypt_of_the_necrodancer_en_1_29_14917.sh'
+ARCHIVE_GOG_3_URL='https://www.gog.com/game/crypt_of_the_necrodancer'
+ARCHIVE_GOG_3_MD5='70d3e29a2a48901d02541d8b1c6326ba'
+ARCHIVE_GOG_3_SIZE='1600000'
+ARCHIVE_GOG_3_VERSION='1.29-gog14917'
+ARCHIVE_GOG_3_TYPE='mojosetup'
 
-ARCHIVE_GOG_OLD='gog_crypt_of_the_necrodancer_2.4.0.7.sh'
-ARCHIVE_GOG_OLD_MD5='a8c21ce12e7e4c769aaddd76321672e4'
-ARCHIVE_GOG_OLD_SIZE='1700000'
-ARCHIVE_GOG_OLD_VERSION='1.28-gog2.4.0.7'
+ARCHIVE_GOG_2='gog_crypt_of_the_necrodancer_2.4.0.7.sh'
+ARCHIVE_GOG_2_MD5='a8c21ce12e7e4c769aaddd76321672e4'
+ARCHIVE_GOG_2_SIZE='1700000'
+ARCHIVE_GOG_2_VERSION='1.28-gog2.4.0.7'
 
-ARCHIVE_GOG_OLDER='gog_crypt_of_the_necrodancer_2.3.0.6.sh'
-ARCHIVE_GOG_OLDER_MD5='bece155772937aa32d2b4eba3aac0dd0'
-ARCHIVE_GOG_OLDER_SIZE='1500000'
-ARCHIVE_GOG_OLDER_VERSION='1.27-gog2.3.0.6'
+ARCHIVE_GOG_1='gog_crypt_of_the_necrodancer_2.3.0.6.sh'
+ARCHIVE_GOG_1_MD5='bece155772937aa32d2b4eba3aac0dd0'
+ARCHIVE_GOG_1_SIZE='1500000'
+ARCHIVE_GOG_1_VERSION='1.27-gog2.3.0.6'
 
-ARCHIVE_GOG_OLDEST='gog_crypt_of_the_necrodancer_2.3.0.5.sh'
-ARCHIVE_GOG_OLDEST_MD5='8a6e7c3d26461aa2fa959b8607e676f7'
-ARCHIVE_GOG_OLDEST_SIZE='1500000'
-ARCHIVE_GOG_OLDEST_VERSION='1.27-gog2.3.0.5'
+ARCHIVE_GOG_0='gog_crypt_of_the_necrodancer_2.3.0.5.sh'
+ARCHIVE_GOG_0_MD5='8a6e7c3d26461aa2fa959b8607e676f7'
+ARCHIVE_GOG_0_SIZE='1500000'
+ARCHIVE_GOG_0_VERSION='1.27-gog2.3.0.5'
 
-ARCHIVE_ICONS_PACK='crypt-of-the-necrodancer_icons.tar.gz'
-ARCHIVE_ICONS_PACK_MD5='04d2bb19adc13dbadce6161bd92bf59a'
+ARCHIVE_DOC0_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC0_DATA_FILES='*'
 
-ARCHIVE_DOC1_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC1_DATA_FILES='./*'
-
-ARCHIVE_DOC2_DATA_PATH='data/noarch/game/'
-ARCHIVE_DOC2_DATA_FILES='./license.txt'
+ARCHIVE_DOC1_DATA_PATH='data/noarch/game/'
+ARCHIVE_DOC1_DATA_FILES='license.txt'
 
 ARCHIVE_GAME_BIN_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN_FILES='./*.so.* ./fmod ./NecroDancer ./essentia*'
+ARCHIVE_GAME_BIN_FILES='NecroDancer essentia* fmod libavcodec.so.53 libavformat.so.53 libavutil.so.51 libfftw3f.so.3 libglfw.so.2 libgsm.so.1 libsamplerate.so.0 libschroedinger-1.0.so.0 libtag.so.1 libyaml-0.so.2'
 
 ARCHIVE_GAME_MUSIC_PATH='data/noarch/game'
-ARCHIVE_GAME_MUSIC_FILES='./data/music'
+ARCHIVE_GAME_MUSIC_FILES='data/music'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='./data/*.png ./data/*.xml ./data/bestiary ./data/entities ./data/essentia ./data/gui ./data/items ./data/languages ./data/level ./data/lua ./data/mainmenu ./data/mentor ./data/particles ./data/sounds* ./data/spells ./data/swipes ./data/text ./data/traps ./data/video'
-
-ARCHIVE_ICONS_PATH='.'
-ARCHIVE_ICONS_FILES='./16x16 ./32x32 ./128x128 ./256x256'
+ARCHIVE_GAME_DATA_FILES='data'
 
 DATA_DIRS='./data/custom_music ./downloaded_dungeons ./downloaded_mods ./logs ./mods ./replays'
-DATA_FILES='./data/save_data.xml ./data/played.dat'
+DATA_FILES='data/save_data.xml data/played.dat'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_LIBS='.'
 APP_MAIN_EXE='NecroDancer'
-APP_MAIN_ICON_GOG='data/noarch/support/icon.png'
-APP_MAIN_ICON_GOG_RES='256'
+APP_MAIN_ICON='data/noarch/support/icon.png'
 
 PACKAGES_LIST='PKG_MUSIC PKG_DATA PKG_BIN'
 
@@ -105,12 +103,20 @@ PKG_DATA_PROVIDE="${GAME_ID}-video"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_MUSIC_ID, $PKG_DATA_ID, libc6, libstdc++6, libgl1-mesa-glx | libgl1, libxrandr2, libopenal1, libvorbis0a"
-PKG_BIN_DEPS_ARCH="$PKG_MUSIC_ID $PKG_DATA_ID lib32-glibc lib32-gcc-libs lib32-libgl lib32-libxrandr lib32-openal lib32-libogg lib32-libvorbis"
+PKG_BIN_DEPS="$PKG_MUSIC_ID $PKG_DATA_ID glibc libstdc++ glx libxrandr openal vorbis"
+
+# Optional icons pack
+
+ARCHIVE_OPTIONAL_ICONS='crypt-of-the-necrodancer_icons.tar.gz'
+ARCHIVE_OPTIONAL_ICONS_MD5='04d2bb19adc13dbadce6161bd92bf59a'
+ARCHIVE_OPTIONAL_ICONS_URL='https://downloads.dotslashplay.it/resources/crypt-of-the-necrodancer/'
+
+ARCHIVE_ICONS_PATH='.'
+ARCHIVE_ICONS_FILES='16x16 32x32 128x128 256x256'
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.12'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
@@ -133,44 +139,45 @@ if [ -z "$PLAYIT_LIB2" ]; then
 	printf 'libplayit2.sh not found.\n'
 	exit 1
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
-# Try to load icons archive
+# Include optional icons pack
 
 ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'ARCHIVE_ICONS' 'ARCHIVE_ICONS_PACK'
+set_archive 'ARCHIVE_ICONS' 'ARCHIVE_OPTIONAL_ICONS'
+if [ -n "$ARCHIVE_ICONS" ]; then
+	unset APP_MAIN_ICON
+	PKG='PKG_DATA'
+	(
+		ARCHIVE='ARCHIVE_ICONS'
+		extract_data_from "$ARCHIVE_ICONS"
+	)
+	organize_data 'ICONS' "$PATH_ICON_BASE"
+	rm --recursive "$PLAYIT_WORKDIR/gamedata"
+fi
 ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-if [ "$ARCHIVE_ICONS" ]; then
-	(
-		ARCHIVE='ARCHIVE_ICONS'
-		extract_data_from "$ARCHIVE_ICONS"
-	)
+prepare_package_layout
+
+# Include game icon
+
+if [ -z "$ARCHIVE_ICONS" ]; then
+	PKG='PKG_DATA'
+	icons_get_from_workdir 'APP_MAIN'
 fi
 
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC1_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "DOC2_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
-
-PKG='PKG_DATA'
-if [ "$ARCHIVE_ICONS" ]; then
-	organize_data 'ICONS' "$PATH_ICON_BASE"
-else
-	get_icon_from_temp_dir 'APP_MAIN'
-fi
+# Clean up temporary files
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
