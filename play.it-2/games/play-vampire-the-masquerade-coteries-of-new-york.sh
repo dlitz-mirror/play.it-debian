d10acc9db5ac20e4a89636cc55445b016291fb27
Mopi <mopi@dotslashplay.it>
2020-10-06 13:17:28 +0200

    New game: Vampire: The Masquerade - Coteries of New York

---
+++ play.it-2/games/play-vampire-the-masquerade-coteries-of-new-york.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+set -o errexit
+
+###
+# Copyright (c) 2015-2020, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2020, Mopi
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Vampire: the Masquerade - Coteries of New York
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
+###
+
+script_version=20201014.1
+
+# Set game-specific variables
+
+GAME_ID='vampire-the-masquerade-coteries-of-new-york'
+GAME_NAME='Vampire: the Masquerade - Coteries of New York'
+
+ARCHIVES_LIST='
+ARCHIVE_GOG_0'
+
+ARCHIVE_GOG_0='vampire_the_masquerade_coteries_of_new_york_1_0_7_40980.sh'
+ARCHIVE_GOG_0_URL='https://www.gog.com/game/vampire_the_masquerade_coteries_of_new_york'
+ARCHIVE_GOG_0_MD5='146113ea6b7295104413d8ce2b1fbf4e'
+ARCHIVE_GOG_0_SIZE='3200000'
+ARCHIVE_GOG_0_VERSION='1.0.7-gog40980'
+ARCHIVE_GOG_0_TYPE='mojosetup'
+
+ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
+ARCHIVE_GAME_DATA_FILES='*'
+
+ARCHIVE_GAME_BIN_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN_FILES='VtM?Coteries?of?New?York.x86_64 VtM?Coteries?of?New?York_Data/MonoBleedingEdge/x86_64 VtM?Coteries?of?New?York_Data/Managed VtM?Coteries?of?New?York_Data/Plugins LinuxPlayer_s.debug UnityPlayer_s.debug UnityPlayer.so'
+
+ARCHIVE_GAME_DATA_PATH='data/noarch/game'
+ARCHIVE_GAME_DATA_FILES='VtM?Coteries?of?New?York_Data'
+
+DATA_DIRS='./logs'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_PRERUN='# Work around Unity3D poor support for non-US locales
+export LANG=C'
+APP_MAIN_EXE='VtM Coteries of New York.x86_64'
+APP_MAIN_ICON='VtM Coteries of New York_Data/Resources/UnityPlayer.png'
+# Use a per-session dedicated file for logs
+# shellcheck disable=SC2016
+APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='64'
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++"
+
+# Load common functions
+
+target_version='2.12'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Get icon
+
+PKG='PKG_DATA'
+icons_get_from_package 'APP_MAIN'
+
+# Clean up temporary directories
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+launchers_write 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
