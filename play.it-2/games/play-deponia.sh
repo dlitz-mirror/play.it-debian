0d425251f02fdbc827989fe988f67563630a690a
vv221 <vv221@dotslashplay.it>
2020-11-24 09:28:10 +0100

    Deponia — Syntax update

--- play.it-2/games/play-deponia.sh
+++ play.it-2/games/play-deponia.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2015-2020, Antoine Le Gonidec <vv221@dotslashplay.it>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,51 +30,53 @@ set -o errexit
 
 ###
 # Deponia
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200918.1
+script_version=20201124.1
 
 # Set game-specific variables
 
 GAME_ID='deponia'
 GAME_NAME='Deponia'
 
-ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_HUMBLE'
+ARCHIVES_LIST='
+ARCHIVE_GOG_0
+ARCHIVE_HUMBLE_0'
 
-ARCHIVE_GOG='gog_deponia_2.1.0.3.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/deponia'
-ARCHIVE_GOG_MD5='a3a21ba1c1ee68c9be2c755bd79e1b30'
-ARCHIVE_GOG_SIZE='1800000'
-ARCHIVE_GOG_VERSION='3.3.1357-gog2.1.0.3'
+ARCHIVE_GOG_0='gog_deponia_2.1.0.3.sh'
+ARCHIVE_GOG_0_MD5='a3a21ba1c1ee68c9be2c755bd79e1b30'
+ARCHIVE_GOG_0_SIZE='1800000'
+ARCHIVE_GOG_0_VERSION='3.3.1357-gog2.1.0.3'
+ARCHIVE_GOG_0_URL='https://www.gog.com/game/deponia'
 
-ARCHIVE_HUMBLE='Deponia_3.3.1358_Full_DEB_Multi_Daedalic_ESD.tar.gz'
-ARCHIVE_HUMBLE_MD5='8ff4e21bbb4abcdc4059845acf7c7f04'
-ARCHIVE_HUMBLE_SIZE='1700000'
-ARCHIVE_HUMBLE_VERSION='3.3.1358-humble160511'
+ARCHIVE_HUMBLE_0='Deponia_3.3.1358_Full_DEB_Multi_Daedalic_ESD.tar.gz'
+ARCHIVE_HUMBLE_0_MD5='8ff4e21bbb4abcdc4059845acf7c7f04'
+ARCHIVE_HUMBLE_0_VERSION='3.3.1358-humble160511'
+ARCHIVE_HUMBLE_0_SIZE='1700000'
 
-ARCHIVE_DOC_PATH_GOG='data/noarch/docs'
-ARCHIVE_DOC_PATH_HUMBLE='Deponia'
-ARCHIVE_DOC_FILES='./documents ./version.txt ./readme.txt'
+ARCHIVE_DOC_DATA_PATH_GOG='data/noarch/docs'
+ARCHIVE_DOC_DATA_PATH_HUMBLE='Deponia'
+ARCHIVE_DOC_DATA_FILES='documents version.txt readme.txt'
 
-ARCHIVE_GAME_BIN_PATH_HUMBLE='Deponia'
 ARCHIVE_GAME_BIN_PATH_GOG='data/noarch/game'
-ARCHIVE_GAME_BIN_FILES='./config.ini ./Deponia libs64/libavcodec.so.56 libs64/libavdevice.so.56 libs64/libavfilter.so.5 libs64/libavformat.so.56 libs64/libavutil.so.54 libs64/libswresample.so.1 libs64/libswscale.so.3 libs64/libz.so.1'
+ARCHIVE_GAME_BIN_PATH_HUMBLE='Deponia'
+ARCHIVE_GAME_BIN_FILES='config.ini Deponia libs64/libavcodec.so.56 libs64/libavdevice.so.56 libs64/libavfilter.so.5 libs64/libavformat.so.56 libs64/libavutil.so.54 libs64/libswresample.so.1 libs64/libswscale.so.3 libs64/libz.so.1'
 
-ARCHIVE_GAME_VIDEOS_PATH_HUMBLE='Deponia'
 ARCHIVE_GAME_VIDEOS_PATH_GOG='data/noarch/game'
-ARCHIVE_GAME_VIDEOS_FILES='./videos'
+ARCHIVE_GAME_VIDEOS_PATH_HUMBLE='Deponia'
+ARCHIVE_GAME_VIDEOS_FILES='videos'
 
-ARCHIVE_GAME_DATA_PATH_HUMBLE='Deponia'
 ARCHIVE_GAME_DATA_PATH_GOG='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='./characters ./data.vis ./lua ./scenes'
+ARCHIVE_GAME_DATA_PATH_HUMBLE='Deponia'
+ARCHIVE_GAME_DATA_FILES='characters data.vis lua scenes'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='Deponia'
 APP_MAIN_LIBS='libs64'
+# No icon is provided by the Humble Bundle archive
 APP_MAIN_ICON_GOG='data/noarch/support/icon.png'
-APP_MAIN_ICON_GOG_RES='256'
 
 PACKAGES_LIST='PKG_VIDEOS PKG_DATA PKG_BIN'
 
@@ -85,21 +87,21 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='64'
-PKG_BIN_DEPS_DEB="$PKG_VIDEOS_ID, $PKG_DATA_ID, libc6, libstdc++6, libgl1-mesa-glx | libgl1, libopenal1, libavcodec56 | libavcodec-ffmpeg56 | libavcodec-extra-56 | libavcodec-ffmpeg-extra56, libavformat56 | libavformat-ffmpeg56, libavutil54 | libavutil-ffmpeg54, libswscale3 | libswscale-ffmpeg3"
-PKG_BIN_DEPS_ARCH="$PKG_VIDEOS_ID $PKG_DATA_ID libgl openal ffmpeg ffmpeg2.8"
+PKG_BIN_DEPS="$PKG_VIDEO_ID $PKG_DATA_ID glbic libsdtc++ glx openal"
+PKG_BIN_DEPS_ARCH='ffmpeg ffmpeg2.8'
+PKG_BIN_DEPS_DEB='libavcodec56 | libavcodec-ffmpeg56 | libavcodec-extra-56 | libavcodec-ffmpeg-extra56, libavformat56 | libavformat-ffmpeg56, libavutil54 | libavutil-ffmpeg54, libswscale3 | libswscale-ffmpeg3'
 
 # Load common functions
 
-target_version='2.4'
+target_version='2.12'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
 		if [ -e "$path/libplayit2.sh" ]; then
@@ -113,31 +115,32 @@ if [ -z "$PLAYIT_LIB2" ]; then
 	printf 'libplayit2.sh not found.\n'
 	exit 1
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
 set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
+prepare_package_layout
 
-PKG='PKG_BIN'
-organize_data 'GAME_BIN' "$PATH_GAME"
+# Get game icon
 
-PKG='PKG_VIDEOS'
-organize_data 'GAME_VIDEOS' "$PATH_GAME"
+case "$ARCHIVE" in
+	('ARCHIVE_GOG_'*)
+		PKG='PKG_DATA'
+		icons_get_from_workdir 'APP_MAIN'
+	;;
+esac
 
-PKG='PKG_DATA'
-organize_data 'DOC'       "$PATH_DOC"
-organize_data 'GAME_DATA' "$PATH_GAME"
-get_icon_from_temp_dir 'APP_MAIN'
+# Clean up temporary files
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
