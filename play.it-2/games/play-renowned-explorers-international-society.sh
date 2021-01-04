d0ad8dcca6bdb290207c7369b3211bd22ab21b2d
vv221 <vv221@dotslashplay.it>
2021-01-04 20:56:19 +0100

    Renowned Explorers — Syntax update

--- play.it-2/games/play-renowned-explorers-international-society.sh
+++ play.it-2/games/play-renowned-explorers-international-society.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
-# Copyright (c) 2016-2020, Mopi
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2021, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,76 +32,76 @@ set -o errexit
 ###
 # Renowned Explorers: International Society
 # build native packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20190619.2
+script_version=20210515.2
 
 # Set game-specific variables
 
 GAME_ID='renowned-explorers-international-society'
 GAME_NAME='Renowned Explorers: International Society'
 
-ARCHIVE_GOG='renowned_explorers_international_society_522_26056.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/renowned_explorers'
-ARCHIVE_GOG_MD5='fe38ae1c4dc2607923cc2a60019bff38'
-ARCHIVE_GOG_SIZE='1200000'
-ARCHIVE_GOG_VERSION='522-gog26056'
-ARCHIVE_GOG_TYPE='mojosetup'
+ARCHIVE_BASE_9='renowned_explorers_international_society_522_26056.sh'
+ARCHIVE_BASE_9_MD5='fe38ae1c4dc2607923cc2a60019bff38'
+ARCHIVE_BASE_9_TYPE='mojosetup'
+ARCHIVE_BASE_9_SIZE='1200000'
+ARCHIVE_BASE_9_VERSION='522-gog26056'
+ARCHIVE_BASE_9_URL='https://www.gog.com/game/renowned_explorers'
 
-ARCHIVE_GOG_OLD8='renowned_explorers_international_society_520_25983.sh'
-ARCHIVE_GOG_OLD8_MD5='2af1dedb29ac1b929971cc0912722760'
-ARCHIVE_GOG_OLD8_SIZE='1200000'
-ARCHIVE_GOG_OLD8_VERSION='520-gog25983'
-ARCHIVE_GOG_OLD8_TYPE='mojosetup'
+ARCHIVE_BASE_8='renowned_explorers_international_society_520_25983.sh'
+ARCHIVE_BASE_8_MD5='2af1dedb29ac1b929971cc0912722760'
+ARCHIVE_BASE_8_TYPE='mojosetup'
+ARCHIVE_BASE_8_SIZE='1200000'
+ARCHIVE_BASE_8_VERSION='520-gog25983'
 
-ARCHIVE_GOG_OLD7='renowned_explorers_international_society_516_25864.sh'
-ARCHIVE_GOG_OLD7_MD5='d868d4b76613b93a94650b750a52752f'
-ARCHIVE_GOG_OLD7_SIZE='1200000'
-ARCHIVE_GOG_OLD7_VERSION='516-gog25864'
-ARCHIVE_GOG_OLD7_TYPE='mojosetup'
+ARCHIVE_BASE_7='renowned_explorers_international_society_516_25864.sh'
+ARCHIVE_BASE_7_MD5='d868d4b76613b93a94650b750a52752f'
+ARCHIVE_BASE_7_TYPE='mojosetup'
+ARCHIVE_BASE_7_SIZE='1200000'
+ARCHIVE_BASE_7_VERSION='516-gog25864'
 
-ARCHIVE_GOG_OLD6='renowned_explorers_international_society_512_25169.sh'
-ARCHIVE_GOG_OLD6_MD5='3f2eb242da5200a78c53162d152a3cac'
-ARCHIVE_GOG_OLD6_SIZE='1100000'
-ARCHIVE_GOG_OLD6_VERSION='512-gog25169'
-ARCHIVE_GOG_OLD6_TYPE='mojosetup'
+ARCHIVE_BASE_6='renowned_explorers_international_society_512_25169.sh'
+ARCHIVE_BASE_6_MD5='3f2eb242da5200a78c53162d152a3cac'
+ARCHIVE_BASE_6_TYPE='mojosetup'
+ARCHIVE_BASE_6_SIZE='1100000'
+ARCHIVE_BASE_6_VERSION='512-gog25169'
 
-ARCHIVE_GOG_OLD5='renowned_explorers_international_society_508_23701.sh'
-ARCHIVE_GOG_OLD5_MD5='247551613c7aba4b4b31f7a98fa31949'
-ARCHIVE_GOG_OLD5_SIZE='1100000'
-ARCHIVE_GOG_OLD5_VERSION='508-gog23701'
-ARCHIVE_GOG_OLD5_TYPE='mojosetup'
+ARCHIVE_BASE_5='renowned_explorers_international_society_508_23701.sh'
+ARCHIVE_BASE_5_MD5='247551613c7aba4b4b31f7a98fa31949'
+ARCHIVE_BASE_5_TYPE='mojosetup'
+ARCHIVE_BASE_5_SIZE='1100000'
+ARCHIVE_BASE_5_VERSION='508-gog23701'
 
-ARCHIVE_GOG_OLD4='renowned_explorers_international_society_503_23529.sh'
-ARCHIVE_GOG_OLD4_MD5='6b7555749bc89cc3dda223e2d43bd838'
-ARCHIVE_GOG_OLD4_SIZE='1100000'
-ARCHIVE_GOG_OLD4_VERSION='503-gog23529'
-ARCHIVE_GOG_OLD4_TYPE='mojosetup'
+ARCHIVE_BASE_4='renowned_explorers_international_society_503_23529.sh'
+ARCHIVE_BASE_4_MD5='6b7555749bc89cc3dda223e2d43bd838'
+ARCHIVE_BASE_4_TYPE='mojosetup'
+ARCHIVE_BASE_4_SIZE='1100000'
+ARCHIVE_BASE_4_VERSION='503-gog23529'
 
-ARCHIVE_GOG_OLD3='renowned_explorers_international_society_en_489_21590.sh'
-ARCHIVE_GOG_OLD3_MD5='9fb2cbe095d437d788eb8ec6402db20b'
-ARCHIVE_GOG_OLD3_SIZE='1100000'
-ARCHIVE_GOG_OLD3_VERSION='489-gog21590'
-ARCHIVE_GOG_OLD3_TYPE='mojosetup'
+ARCHIVE_BASE_3='renowned_explorers_international_society_en_489_21590.sh'
+ARCHIVE_BASE_3_MD5='9fb2cbe095d437d788eb8ec6402db20b'
+ARCHIVE_BASE_3_TYPE='mojosetup'
+ARCHIVE_BASE_3_SIZE='1100000'
+ARCHIVE_BASE_3_VERSION='489-gog21590'
 
-ARCHIVE_GOG_OLD2='renowned_explorers_international_society_en_489_20916.sh'
-ARCHIVE_GOG_OLD2_MD5='42d0ecb54d8302545e78f41ed43acef6'
-ARCHIVE_GOG_OLD2_SIZE='1100000'
-ARCHIVE_GOG_OLD2_VERSION='489-gog20916'
-ARCHIVE_GOG_OLD2_TYPE='mojosetup'
+ARCHIVE_BASE_2='renowned_explorers_international_society_en_489_20916.sh'
+ARCHIVE_BASE_2_MD5='42d0ecb54d8302545e78f41ed43acef6'
+ARCHIVE_BASE_2_TYPE='mojosetup'
+ARCHIVE_BASE_2_SIZE='1100000'
+ARCHIVE_BASE_2_VERSION='489-gog20916'
 
-ARCHIVE_GOG_OLD1='renowned_explorers_international_society_en_466_15616.sh'
-ARCHIVE_GOG_OLD1_MD5='fbad4b4d361a0e7d29b9781e3c5a5e85'
-ARCHIVE_GOG_OLD1_SIZE='1100000'
-ARCHIVE_GOG_OLD1_VERSION='466-gog15616'
-ARCHIVE_GOG_OLD1_TYPE='mojosetup'
+ARCHIVE_BASE_1='renowned_explorers_international_society_en_466_15616.sh'
+ARCHIVE_BASE_1_MD5='fbad4b4d361a0e7d29b9781e3c5a5e85'
+ARCHIVE_BASE_1_TYPE='mojosetup'
+ARCHIVE_BASE_1_SIZE='1100000'
+ARCHIVE_BASE_1_VERSION='466-gog15616'
 
-ARCHIVE_GOG_OLD0='renowned_explorers_international_society_en_459_14894.sh'
-ARCHIVE_GOG_OLD0_MD5='ff6b368b3919002d2db750213d33fcef'
-ARCHIVE_GOG_OLD0_SIZE='1100000'
-ARCHIVE_GOG_OLD0_VERSION='459-gog14894'
-ARCHIVE_GOG_OLD0_TYPE='mojosetup'
+ARCHIVE_BASE_0='renowned_explorers_international_society_en_459_14894.sh'
+ARCHIVE_BASE_0_MD5='ff6b368b3919002d2db750213d33fcef'
+ARCHIVE_BASE_0_TYPE='mojosetup'
+ARCHIVE_BASE_0_SIZE='1100000'
+ARCHIVE_BASE_0_VERSION='459-gog14894'
 
 ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
 ARCHIVE_DOC_DATA_FILES='*'
@@ -131,33 +131,26 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx glu xcursor sdl2 libxrandr"
-PKG_BIN32_DEPS_ARCH='lib32-libx11'
-PKG_BIN32_DEPS_DEB='libx11-6'
-PKG_BIN32_DEPS_GENTOO='x11-libs/libX11[abi_x86_32]'
+PKG_BIN32_DEPS="${PKG_DATA_ID} glibc libstdc++ glx xcursor libxrandr libX11.so.6 libGLU.so.1 libSDL2-2.0.so.0"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
-PKG_BIN64_DEPS_ARCH='libx11'
-PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
-PKG_BIN64_DEPS_GENTOO='x11-libs/libX11'
 
 # Load common functions
 
-target_version='2.11'
+target_version='2.13'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
-		if [ -e "$path/libplayit2.sh" ]; then
-			PLAYIT_LIB2="$path/libplayit2.sh"
+		if [ -e "${path}/libplayit2.sh" ]; then
+			PLAYIT_LIB2="${path}/libplayit2.sh"
 			break
 		fi
 	done
@@ -179,7 +172,10 @@ prepare_package_layout
 
 PKG='PKG_DATA'
 icons_get_from_workdir 'APP_MAIN'
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Clean up temporary files
+
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
 
 # Write launchers
 
