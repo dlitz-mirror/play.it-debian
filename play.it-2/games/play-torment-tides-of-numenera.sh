381faca980d9f9aa84a90e020d55638ca5c74e83
vv221 <vv221@dotslashplay.it>
2021-01-18 23:26:19 +0100

    Torment: Tides of Numenera — Syntax update

--- play.it-2/games/play-torment-tides-of-numenera.sh
+++ play.it-2/games/play-torment-tides-of-numenera.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -34,18 +34,19 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200201.1
+script_version=20210515.2
 
 # Set game-specific variables
 
 GAME_ID='torment-tides-of-numenera'
 GAME_NAME='Torment: Tides of Numenera'
 
-ARCHIVE_GOG='gog_torment_tides_of_numenera_2.3.0.4.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/torment_tides_of_numenera_legacy_edition_content'
-ARCHIVE_GOG_MD5='839337b42a1618f3b445f363eca210d3'
-ARCHIVE_GOG_SIZE='9300000'
-ARCHIVE_GOG_VERSION='1.1.0-gog2.3.0.4'
+ARCHIVE_BASE_0='gog_torment_tides_of_numenera_2.3.0.4.sh'
+ARCHIVE_BASE_0_MD5='839337b42a1618f3b445f363eca210d3'
+ARCHIVE_BASE_0_TYPE='mojosetup'
+ARCHIVE_BASE_0_SIZE='9300000'
+ARCHIVE_BASE_0_VERSION='1.1.0-gog2.3.0.4'
+ARCHIVE_BASE_0_URL='https://www.gog.com/game/torment_tides_of_numenera_legacy_edition_content'
 
 ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
 ARCHIVE_DOC_DATA_FILES='*'
@@ -62,14 +63,8 @@ ARCHIVE_GAME_RESOURCES_FILES='TidesOfNumenera_Data/resources.assets*'
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
 ARCHIVE_GAME_DATA_FILES='TidesOfNumenera_Data'
 
-DATA_DIRS='./logs'
-
 APP_MAIN_TYPE='native'
-APP_MAIN_PRERUN='# Work around engine bugs on non-US locales
-export LANG=C'
 APP_MAIN_EXE='TidesOfNumenera'
-# shellcheck disable=SC2016
-APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
 APP_MAIN_ICON='TidesOfNumenera_Data/Resources/UnityPlayer.png'
 
 PACKAGES_LIST='PKG_BIN PKG_AUDIO PKG_RESOURCES PKG_DATA'
@@ -82,30 +77,41 @@ PKG_RESOURCES_DESCRIPTION='resources'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
-PKG_DATA_DEPS="$PKG_AUDIO_ID $PKG_RESOURCES_ID"
+PKG_DATA_DEPS="${PKG_AUDIO_ID} ${PKG_RESOURCES_ID}"
 
 PKG_BIN_ARCH='64'
-PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr sdl2 gtk2"
-PKG_BIN_DEPS_ARCH='libx11 gdk-pixbuf2 glib2'
-PKG_BIN_DEPS_DEB='libx11-6, libgdk-pixbuf2.0-0, libglib2.0-0'
-PKG_BIN_DEPS_GENTOO='x11-libs/libX11 x11-libs/gdk-pixbuf dev-libs/glib'
+PKG_BIN_DEPS="${PKG_DATA_ID} glibc libstdc++ glx xcursor libxrandr gtk2 libSDL2-2.0.so.0 libX11.so.6 libgdk_pixbuf-2.0.so.0 libgobject-2.0.so.0 libglib-2.0.so.0"
+
+# Use a per-session dedicated file for logs
+
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Use a per-session dedicated file for logs
+mkdir --parents logs
+APP_OPTIONS="${APP_OPTIONS} -logFile ./logs/$(date +%F-%R).log"'
+
+# Work around Unity3D poor support for non-US locales
+
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Work around Unity3D poor support for non-US locales
+export LANG=C'
 
 # Load common functions
 
-target_version='2.11'
+target_version='2.13'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
-		if [ -e "$path/libplayit2.sh" ]; then
-			PLAYIT_LIB2="$path/libplayit2.sh"
+		if [ -e "${path}/libplayit2.sh" ]; then
+			PLAYIT_LIB2="${path}/libplayit2.sh"
 			break
 		fi
 	done
@@ -122,12 +128,15 @@ fi
 
 extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Get icon
 
 PKG='PKG_DATA'
 icons_get_from_package 'APP_MAIN'
+
+# Delete temporary files
+
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
 
 # Write launchers
 
