164dec77d0dc92f7ed5e8dfdea153de1554b33ed
Mopi <mopi@dotslashplay.it>
2021-02-11 22:39:01 +0100

    New game: Seasons After Fall

---
+++ play.it-2/games/play-seasons-after-fall.sh
@@ -0,0 +1,153 @@
+#!/bin/sh
+set -o errexit
+
+###
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2021, Mopi
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Seasons After Fall
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
+###
+
+script_version=20210416.4
+
+# Set game-specific variables
+
+GAME_ID='seasons-after-fall'
+GAME_NAME='Seasons After Fall'
+
+ARCHIVES_LIST='
+ARCHIVE_BASE_0'
+
+ARCHIVE_BASE_0='setup_seasons_after_fall_26129_(30224).exe'
+ARCHIVE_BASE_0_MD5='f7207d11fe8483d428d23ca4b62615d1'
+ARCHIVE_BASE_0_TYPE='innosetup'
+ARCHIVE_BASE_0_SIZE='4100000'
+ARCHIVE_BASE_0_VERSION='26129-gog30224'
+ARCHIVE_BASE_0_URL='https://www.gog.com/game/seasons_after_fall'
+
+ARCHIVE_GAME_BIN_PATH='.'
+ARCHIVE_GAME_BIN_FILES='seasons?after?fall.exe seasons?after?fall_data/plugins seasons?after?fall_data/mono'
+
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='seasons?after?fall_data'
+
+APP_MAIN_TYPE='wine'
+APP_MAIN_EXE='seasons after fall.exe'
+APP_MAIN_ICON='seasons after fall.exe'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID wine"
+
+# Load common functions
+
+target_version='2.12'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Get icon
+
+PKG='PKG_BIN'
+icons_get_from_package 'APP_MAIN'
+
+# Clean up temporary directories
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Use persistent storage for saved games and settings
+
+CONFIG_FILES="${CONFGI_FILES} ./userdata/UserOptions.json"
+DATA_FILES="${DATA_FILES} ./userdata/*.save"
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Use persistent storage for saved games and settings
+userdata_path_prefix="$WINEPREFIX/drive_c/users/$USER/Saved Games/Seasons after Fall"
+userdata_path_persistent="$PATH_PREFIX/userdata"
+mkdir --parents "$userdata_path_persistent"
+if [ ! -h "$userdata_path_prefix" ]; then
+	if [ -d "$userdata_path_prefix" ]; then
+		# Migrate existing user data to the persistent path
+		mv "$userdata_path_prefix"/* "$userdata_path_persistent"
+		rmdir "$userdata_path_prefix"
+	fi
+	# Create link from prefix to persistent path
+	mkdir --parents "$(dirname "$userdata_path_prefix")"
+	ln --symbolic "$userdata_path_persistent" "$userdata_path_prefix"
+fi'
+
+# Write launchers
+
+PKG='PKG_BIN'
+launchers_write 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
