46bf97b39e01d2eeb5968894ae67949a45558ec9
vv221 <vv221@dotslashplay.it>
2021-02-17 22:19:21 +0100

    Litil Divil — Syntax update

--- play.it-2/games/play-litil-divil.sh
+++ play.it-2/games/play-litil-divil.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dtoslashplay.it>
+# Copyright (c) 2016-2021, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,37 +31,38 @@ set -o errexit
 
 ###
 # Litil Divil
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200918.1
+script_version=20210217.1
 
 # Set game-specific variables
 
 GAME_ID='litil-divil'
 GAME_NAME='Litil Divil'
 
-ARCHIVES_LIST='ARCHIVE_GOG'
+ARCHIVES_LIST='
+ARCHIVE_GOG_0'
 
-ARCHIVE_GOG='gog_litil_divil_2.0.0.21.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/litil_divil'
-ARCHIVE_GOG_MD5='1258be406cb4b40c912c4846df2ac92b'
-ARCHIVE_GOG_SIZE='44000'
-ARCHIVE_GOG_VERSION='1.0-gog2.0.0.21'
-ARCHIVE_GOG_TYPE='mojosetup'
+ARCHIVE_GOG_0='gog_litil_divil_2.0.0.21.sh'
+ARCHIVE_GOG_0_MD5='1258be406cb4b40c912c4846df2ac92b'
+ARCHIVE_GOG_0_TYPE='mojosetup'
+ARCHIVE_GOG_0_SIZE='44000'
+ARCHIVE_GOG_0_VERSION='1.0-gog2.0.0.21'
+ARCHIVE_GOG_0_URL='https://www.gog.com/game/litil_divil'
 
-ARCHIVE_DOC1_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC1_DATA_FILES='./*.txt ./*.pdf'
+ARCHIVE_DOC0_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC0_DATA_FILES='*.txt *.pdf'
 
-ARCHIVE_DOC2_DATA_PATH='data/noarch/data'
-ARCHIVE_DOC2_DATA_FILES='./config.doc'
+ARCHIVE_DOC1_DATA_PATH='data/noarch/data'
+ARCHIVE_DOC1_DATA_FILES='config.doc'
 
 ARCHIVE_GAME_BIN_PATH='data/noarch/data'
-ARCHIVE_GAME_BIN_FILES='./config.exe ./data/divil.exe ./divils.cfg'
+ARCHIVE_GAME_BIN_FILES='config.exe data/divil.exe divils.cfg'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/data'
-ARCHIVE_GAME_DATA_FILES='./gfx ./data/*.map ./data/gfx ./data/level*'
+ARCHIVE_GAME_DATA_FILES='gfx data/*.map data/gfx data/level*'
 
 CONFIG_FILES='./divils.cfg'
 
@@ -72,13 +74,13 @@ APP_MAIN_PRERUN='d:'
 APP_MAIN_EXE='divil.exe'
 APP_MAIN_OPTIONS='c:'
 APP_MAIN_ICON='data/noarch/support/icon.png'
-APP_MAIN_ICON_RES='512'
 
 APP_CONFIG_ID="${GAME_ID}_config"
-APP_CONFIG_NAME="$GAME_NAME - configuration"
+APP_CONFIG_NAME="${GAME_NAME} - configuration"
+APP_CONFIG_CAT='Settings'
 APP_CONFIG_TYPE='dosbox'
 APP_CONFIG_EXE='config.exe'
-APP_CONFIG_CAT='Settings'
+APP_CONFIG_ICON='data/noarch/support/icon.png'
 
 PACKAGES_LIST='PKG_BIN PKG_DATA'
 
@@ -86,20 +88,19 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID dosbox"
+PKG_BIN_DEPS="${PKG_DATA_ID} dosbox"
 
 # Load common functions
 
-target_version='2.4'
+target_version='2.12'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
 		if [ -e "$path/libplayit2.sh" ]; then
@@ -113,35 +114,28 @@ if [ -z "$PLAYIT_LIB2" ]; then
 	printf 'libplayit2.sh not found.\n'
 	exit 1
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-tolower "$PLAYIT_WORKDIR/gamedata"
+tolower "${PLAYIT_WORKDIR}/gamedata"
+prepare_package_layout
 
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC1_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "DOC2_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
+# Get game icon
 
 PKG='PKG_DATA'
-get_icon_from_temp_dir 'APP_MAIN'
+icons_get_from_workdir 'APP_MAIN' 'APP_CONFIG'
 
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+# Clean up temporary files
+
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN' 'APP_CONFIG'
-
-# Use base game icon for settings launcher
-
-file="${PKG_BIN_PATH}${PATH_DESK}/$APP_CONFIG_ID.desktop"
-regex="s/\\(Icon=\\).\\+/\\1$GAME_ID/"
-sed --in-place "$regex" "$file"
+launchers_write 'APP_MAIN' 'APP_CONFIG'
 
 # Build package
 
@@ -150,7 +144,7 @@ build_pkg
 
 # Clean up
 
-rm --recursive "${PLAYIT_WORKDIR}"
+rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
