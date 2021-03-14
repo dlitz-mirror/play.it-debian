0684b47e2fe4c37c04ab92aca947c5047628cad6
vv221 <vv221@dotslashplay.it>
2021-03-14 17:35:29 +0100

    New game: Aztez

---
+++ play.it-2/games/play-aztez.sh
@@ -0,0 +1,198 @@
+#!/bin/sh
+set -o errexit
+
+###
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Aztez
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
+###
+
+script_version=20210519.3
+
+# Set game-specific variables
+
+GAME_ID='aztez'
+GAME_NAME='Aztez'
+
+ARCHIVE_BASE_0='aztez_linux_8278.zip'
+ARCHIVE_BASE_0_MD5='a55092525d52960ad36932918dcadc5b'
+ARCHIVE_BASE_0_SIZE='710000'
+ARCHIVE_BASE_0_VERSION='1.02.8278-humble.2017.09.04'
+ARCHIVE_BASE_0_URL='https://www.humblebundle.com/store/aztez'
+
+ARCHIVE_GAME_BIN_PATH='.'
+ARCHIVE_GAME_BIN_FILES='Aztez.x86 Aztez_Data/Mono Aztez_Data/Plugins'
+
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='Aztez_Data'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE='Aztez.x86'
+APP_MAIN_ICON='Aztez_Data/Resources/UnityPlayer.png'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="${PKG_DATA_ID} glibc libstdc++ gtk2 glx libgdk_pixbuf-2.0.so.0 libgobject-2.0.so.0 libglib-2.0.so.0 libasound.so.2"
+
+# Use a per-session dedicated file for logs
+
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Use a per-session dedicated file for logs
+mkdir --parents logs
+APP_OPTIONS="${APP_OPTIONS} -logFile ./logs/$(date +%F-%R).log"'
+
+# Work around crash on launch related to libpulse
+
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Start pulseaudio if it is available
+if command -v pulseaudio >/dev/null 2>&1; then
+	PULSEAUDIO_IS_AVAILABLE=1
+	if pulseaudio --check; then
+		KEEP_PULSEAUDIO_RUNNING=1
+	else
+		KEEP_PULSEAUDIO_RUNNING=0
+	fi
+	pulseaudio --start
+else
+	PULSEAUDIO_IS_AVAILABLE=0
+fi
+
+# Work around crash on launch related to libpulse
+# Some Unity3D games crash on launch if libpulse-simple.so.0 is available but pulseaudio is not running
+if [ $PULSEAUDIO_IS_AVAILABLE -eq 0 ]; then
+	mkdir --parents "${APP_LIBS:=libs}"
+	ln --force --symbolic /dev/null "$APP_LIBS/libpulse-simple.so.0"
+else
+	if \
+		[ -h "${APP_LIBS:=libs}/libpulse-simple.so.0" ] && \
+		[ "$(realpath "$APP_LIBS/libpulse-simple.so.0")" = "/dev/null" ]
+	then
+		rm "$APP_LIBS/libpulse-simple.so.0"
+		rmdir --ignore-fail-on-non-empty --parents "$APP_LIBS"
+	fi
+fi
+
+# Do not exit early if the game exits in a failure state
+set +o errexit'
+APP_MAIN_POSTRUN="$APP_MAIN_POSTRUN"'
+
+# Restore exit on error
+set -o errexit
+
+# Stop pulseaudio if it has specifically been started for the game
+if \
+	[ $PULSEAUDIO_IS_AVAILABLE -eq 1 ] && \
+	[ $KEEP_PULSEAUDIO_RUNNING -eq 0 ]
+then
+	pulseaudio --kill
+fi'
+
+# Work around Unity3D poor support for non-US locales
+
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Work around Unity3D poor support for non-US locales
+export LANG=C'
+
+# Load common functions
+
+target_version='2.13'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
+		'/usr/share/play.it'
+	do
+		if [ -e "${path}/libplayit2.sh" ]; then
+			PLAYIT_LIB2="${path}/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+
+# Drop unused x86_64 libraries
+
+rm --force --recursive \
+	"${PLAYIT_WORKDIR}/gamedata/Aztez_Data/Mono/x86_64" \
+	"${PLAYIT_WORKDIR}/gamedata/Aztez_Data/Plugins/x86_64"
+
+# Prepare packages
+
+prepare_package_layout
+
+# Get game icon
+
+PKG='PKG_DATA'
+icons_get_from_package 'APP_MAIN'
+
+# Clean up temporary files
+
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+launchers_write 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
