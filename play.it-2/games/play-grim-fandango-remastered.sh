f8f644c81b660cbf28e5a1f3b88832f5c6ffa449
vv221 <vv221@dotslashplay.it>
2021-03-28 21:26:43 +0200

    Grim Fandango — Syntax update

--- play.it-2/games/play-grim-fandango-remastered.sh
+++ play.it-2/games/play-grim-fandango-remastered.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
-# Copyright (c) 2016-2020, Mopi
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2021, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,28 +32,28 @@ set -o errexit
 ###
 # Grim Fandango Remastered
 # build native packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20191130.3
+script_version=20210328.1
 
 # Set game-specific variables
 
 GAME_ID='grim-fandango'
 GAME_NAME='Grim Fandango Remastered'
 
-ARCHIVE_GOG='gog_grim_fandango_remastered_2.3.0.7.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/grim_fandango_remastered'
-ARCHIVE_GOG_MD5='9c5d124c89521d254b0dc259635b2abe'
-ARCHIVE_GOG_SIZE='6100000'
-ARCHIVE_GOG_VERSION='1.4-gog2.3.0.7'
-ARCHIVE_GOG_TYPE='mojosetup_unzip'
+ARCHIVES_LIST='
+ARCHIVE_GOG_0'
 
-ARCHIVE_DOC0_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC0_DATA_FILES='*'
+ARCHIVE_GOG_0='gog_grim_fandango_remastered_2.3.0.7.sh'
+ARCHIVE_GOG_0_MD5='9c5d124c89521d254b0dc259635b2abe'
+ARCHIVE_GOG_0_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_0_SIZE='6100000'
+ARCHIVE_GOG_0_VERSION='1.4-gog2.3.0.7'
+ARCHIVE_GOG_0_URL='https://www.gog.com/game/grim_fandango_remastered'
 
-ARCHIVE_DOC1_DATA_PATH='data/noarch/game/bin'
-ARCHIVE_DOC1_DATA_FILES='*License.txt common-licenses'
+ARCHIVE_DOC_DATA_PATH='data/noarch/game/bin'
+ARCHIVE_DOC_DATA_FILES='*License.txt common-licenses'
 
 ARCHIVE_GAME_BIN_PATH='data/noarch/game/bin'
 ARCHIVE_GAME_BIN_FILES='GrimFandango *.so x86'
@@ -65,11 +65,6 @@ ARCHIVE_GAME_DATA_PATH='data/noarch/game/bin'
 ARCHIVE_GAME_DATA_FILES='*.lab *.LAB controllerdef.txt en_gagl088.lip FontsHD *.tab icon.png patch_v2_or_v3_to_v4.bin patch_v4_to_v5.bin'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_PRERUN_ARCH='SYSTEM_SDL2_PATH="/usr/lib32/libSDL2-2.0.so.0"'
-APP_MAIN_PRERUN_DEB='SYSTEM_SDL2_PATH="/usr/lib/i386-linux-gnu/libSDL2-2.0.so.0"'
-APP_MAIN_PRERUN_GENTOO='SYSTEM_SDL2_PATH="/usr/lib32/libSDL2-2.0.so.0"'
-APP_MAIN_PRERUN='
-ln --force --symbolic "$SYSTEM_SDL2_PATH" ./libSDL2-2.0.so.1'
 APP_MAIN_EXE='GrimFandango'
 APP_MAIN_ICON='icon.png'
 
@@ -82,23 +77,22 @@ PKG_MOVIES_ID="${GAME_ID}-movies"
 PKG_MOVIES_DESCRIPTION='movies'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_MOVIES_ID $PKG_DATA_ID glibc libstdc++ sdl2 glx glu alsa"
+PKG_BIN_DEPS="${PKG_MOVIES_ID} ${PKG_DATA_ID} glibc libstdc++ sdl2 glx glu alsa"
 PKG_BIN_DEPS_ARCH='lib32-libx11'
 PKG_BIN_DEPS_DEB='libx11-6'
 PKG_BIN_DEPS_GENTOO='x11-libs/libX11[abi_x86_32]'
 
 # Load common functions
 
-target_version='2.11'
+target_version='2.12'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
 		if [ -e "$path/libplayit2.sh" ]; then
@@ -119,30 +113,41 @@ fi
 
 extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Get icon
 
 PKG='PKG_DATA'
 icons_get_from_package 'APP_MAIN'
 
-# Write launchers
+# Clean up temporary files
 
-PKG='PKG_BIN'
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
+
+# Use system-provided SDL2 library
+
 case "$OPTION_PACKAGE" in
 	('arch')
-		APP_MAIN_PRERUN="$APP_MAIN_PRERUN_ARCH $APP_MAIN_PRERUN"
+		SYSTEM_SDL2_PATH='/usr/lib32/libSDL2-2.0.so.0'
 	;;
 	('deb')
-		APP_MAIN_PRERUN="$APP_MAIN_PRERUN_DEB $APP_MAIN_PRERUN"
+		SYSTEM_SDL2_PATH='/usr/lib/i386-linux-gnu/libSDL2-2.0.so.0'
 	;;
 	('gentoo')
-		APP_MAIN_PRERUN="$APP_MAIN_PRERUN_GENTOO $APP_MAIN_PRERUN"
+		SYSTEM_SDL2_PATH='/usr/lib32/libSDL2-2.0.so.0'
 	;;
 	(*)
 		liberror 'OPTION_PACKAGE' "$0"
 	;;
 esac
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Use system-provided SDL2 library
+SYSTEM_SDL2_PATH='"'$SYSTEM_SDL2_PATH'"'
+ln --force --symbolic "$SYSTEM_SDL2_PATH" ./libSDL2-2.0.so.1'
+
+# Write launchers
+
+PKG='PKG_BIN'
 launchers_write 'APP_MAIN'
 
 # Build package
