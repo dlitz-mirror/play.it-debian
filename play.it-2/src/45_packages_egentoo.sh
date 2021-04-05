f03cc3d9185461a5eafb88c2282edf5f09982cdd
Hoël Bézier <hoelbezier@riseup.net>
2021-04-05 12:52:06 +0200

    Copy pasted a few more lines from 45_packages_gentoo.sh

--- play.it-2/src/45_packages_egentoo.sh
+++ play.it-2/src/45_packages_egentoo.sh
@@ -22,8 +22,54 @@ pkg_write_egentoo() {
 
 	ebuild_path="$PLAYIT_WORKDIR/$pkg/$(package_get_id "$pkg")-$(packages_get_version "$ARCHIVE").ebuild"
 
+	case "$(package_get_architecture "$pkg")" in
+		('32')
+			pkg_architectures='-* x86 amd64'
+		;;
+		('64')
+			pkg_architectures='-* amd64'
+		;;
+		(*)
+			pkg_architectures='x86 amd64' # data packages
+		;;
+	esac
+
 	cat > "$ebuild_path" <<- EOF
 	EAPI=7
 	RESTRICT="fetch strip binchecks"
+
+	KEYWORDS="$pkg_architectures"
+	DESCRIPTION="$(package_get_description "$pkg")"
+	SLOT="0"
+
+	RDEPEND="$pkg_deps"
+
+	src_install() {
+		doins -r .
+	}
 	EOF
+
+	if [ -n "$(get_value "${pkg}_POSTINST_RUN")" ]; then
+		cat >> "$ebuild_path" <<- EOF
+		pkg_postinst() {
+		$(get_value "${pkg}_POSTINST_RUN")
+		}
+		EOF
+	# For compatibility with pre-2.12 scripts,
+	# ignored if a package-specific value is already set
+	elif [ -e "$postinst" ]; then
+		compat_pkg_write_gentoo_postinst "$ebuild_path"
+	fi
+
+	if [ -n "$(get_value "${pkg}_PRERM_RUN")" ]; then
+		cat >> "$ebuild_path" <<- EOF
+		pkg_prerm() {
+		$(get_value "${pkg}_PRERM_RUN")
+		}
+		EOF
+	# For compatibility with pre-2.12 scripts,
+	# ignored if a package-specific value is already set
+	elif [ -e "$prerm" ]; then
+		compat_pkg_write_gentoo_prerm "$ebuild_path"
+	fi
 }
