d9fd74d2d2d4b443815ee36f9450594f4cf5c105
Hoël Bézier <hoelbezier@riseup.net>
2021-04-06 14:57:41 +0200

    Replace “pkg” by “package” wherever fit

--- play.it-2/src/45_packages_egentoo.sh
+++ play.it-2/src/45_packages_egentoo.sh
@@ -3,37 +3,37 @@
 # NEEDED VARS: GAME_NAME PKG_DEPS_GENTOO
 # CALLED BY: write_metadata
 pkg_write_egentoo() {
-	local pkg
+	local package
 	local pkg_deps
-	local pkg_filename_base
-	local pkg_architectures
+	local package_filename_base
+	local package_architectures
 	local ebuild_path
 
-	pkg="$1"
+	package="$1"
 
-	use_archive_specific_value "${pkg}_DEPS"
-	if [ "$(get_value "${pkg}_DEPS")" ]; then
+	use_archive_specific_value "${package}_DEPS"
+	if [ "$(get_value "${package}_DEPS")" ]; then
 		#shellcheck disable=SC2046
-		pkg_set_deps_gentoo $(get_value "${pkg}_DEPS")
+		pkg_set_deps_gentoo $(get_value "${package}_DEPS")
 	fi
-	use_archive_specific_value "${pkg}_DEPS_GENTOO"
-	if [ "$(get_value "${pkg}_DEPS_GENTOO")" ]; then
-		pkg_deps="$pkg_deps $(get_value "${pkg}_DEPS_GENTOO")"
+	use_archive_specific_value "${package}_DEPS_GENTOO"
+	if [ "$(get_value "${package}_DEPS_GENTOO")" ]; then
+		pkg_deps="$pkg_deps $(get_value "${package}_DEPS_GENTOO")"
 	fi
-	if [ -n "$(package_get_provide "$pkg")" ]; then
-		pkg_deps="${pkg_deps} $(package_get_provide "$pkg")"
+	if [ -n "$(package_get_provide "$package")" ]; then
+		pkg_deps="${pkg_deps} $(package_get_provide "$package")"
 	fi
 
-	pkg_filename_base="$(package_get_id "$pkg")-$(packages_get_version "$ARCHIVE").tar"
+	package_filename_base="$(package_get_name "$package").tar"
 	case $OPTION_COMPRESSION in
 		('gzip')
-			pkg_filename_base="${pkg_filename_base}.gz"
+			package_filename_base="${package_filename_base}.gz"
 		;;
 		('xz')
-			pkg_filename_base="${pkg_filename_base}.xz"
+			package_filename_base="${package_filename_base}.xz"
 		;;
 		('bzip2')
-			pkg_filename_base="${pkg_filename_base}.bz2"
+			package_filename_base="${package_filename_base}.bz2"
 		;;
 		('none') ;;
 		(*)
@@ -41,20 +41,20 @@ pkg_write_egentoo() {
 		;;
 	esac
 
-	case "$(package_get_architecture "$pkg")" in
+	case "$(package_get_architecture "$package")" in
 		('32')
-			pkg_architectures='-* x86 amd64'
+			package_architectures='-* x86 amd64'
 		;;
 		('64')
-			pkg_architectures='-* amd64'
+			package_architectures='-* amd64'
 		;;
 		(*)
-			pkg_architectures='x86 amd64' # data packages
+			package_architectures='x86 amd64' # data packages
 		;;
 	esac
 
-	mkdir --parents "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$pkg")"
-	ebuild_path=$(realpath "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$pkg")/$(package_get_id "$pkg")-$(packages_get_version "$ARCHIVE").ebuild")
+	mkdir --parents "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$package")"
+	ebuild_path=$(realpath "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$package")/$(package_get_name "$package").ebuild")
 
 	cat > "$ebuild_path" << EOF
 # Copyright 1999-2021 Gentoo Authors
@@ -63,9 +63,9 @@ pkg_write_egentoo() {
 EAPI=7
 RESTRICT="fetch strip binchecks"
 
-KEYWORDS="$pkg_architectures"
-DESCRIPTION="$(package_get_description "$pkg")"
-SRC_URI="$pkg_filename_base"
+KEYWORDS="$package_architectures"
+DESCRIPTION="$(package_get_description "$package")"
+SRC_URI="$package_filename_base"
 SLOT="0"
 
 RDEPEND="$pkg_deps"
@@ -82,11 +82,11 @@ src_install() {
 }
 EOF
 
-	if [ -n "$(get_value "${pkg}_POSTINST_RUN")" ]; then
+	if [ -n "$(get_value "${package}_POSTINST_RUN")" ]; then
 		cat >> "$ebuild_path" << EOF
 
 pkg_postinst() {
-	$(get_value "${pkg}_POSTINST_RUN")
+	$(get_value "${package}_POSTINST_RUN")
 }
 EOF
 
@@ -96,11 +96,11 @@ EOF
 		compat_pkg_write_gentoo_postinst "$ebuild_path"
 	fi
 
-	if [ -n "$(get_value "${pkg}_PRERM_RUN")" ]; then
+	if [ -n "$(get_value "${package}_PRERM_RUN")" ]; then
 		cat >> "$ebuild_path" << EOF
 
 pkg_prerm() {
-	$(get_value "${pkg}_PRERM_RUN")
+	$(get_value "${package}_PRERM_RUN")
 }
 EOF
 
@@ -116,17 +116,17 @@ EOF
 # NEEDED VARS: (LANG) PLAYIT_WORKDIR
 # CALLED BY: build_pkg
 pkg_build_egentoo() {
-	local pkg
-	local pkg_path
-	local pkg_filename
+	local package
+	local package_path
+	local package_filename
 	local tar_options
 
-	pkg="$1"
-	pkg_path=$(get_value "${pkg}_PATH")
+	package="$1"
+	package_path=$(package_get_path "$package")
 
 	# We don’t want both binary packages to overwrite each other
-	mkdir --parents "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$pkg")"
-	pkg_filename=$(realpath "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$pkg")/$(package_get_id "$pkg")-$(packages_get_version "$ARCHIVE").tar")
+	mkdir --parents "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$package")"
+	package_filename=$(realpath "$OPTION_OUTPUT_DIR/$(package_get_architecture_string "$package")/$(package_get_name "$package").tar")
 
 	tar_options='--create'
 	if [ -z "$PLAYIT_TAR_IMPLEMENTATION" ]; then
@@ -147,16 +147,16 @@ pkg_build_egentoo() {
 	case $OPTION_COMPRESSION in
 		('gzip')
 			tar_options="$tar_options --gzip"
-			pkg_filename="${pkg_filename}.gz"
+			package_filename="${package_filename}.gz"
 		;;
 		('xz')
 			export XZ_DEFAULTS="${XZ_DEFAULTS:=--threads=0}"
 			tar_options="$tar_options --xz"
-			pkg_filename="${pkg_filename}.xz"
+			package_filename="${package_filename}.xz"
 		;;
 		('bzip2')
 			tar_options="$tar_options --bzip2"
-			pkg_filename="${pkg_filename}.bz2"
+			package_filename="${package_filename}.bz2"
 		;;
 		('none') ;;
 		(*)
@@ -164,28 +164,28 @@ pkg_build_egentoo() {
 		;;
 	esac
 
-	if [ -e "$pkg_filename" ] && [ $OVERWRITE_PACKAGES -ne 1 ]; then
-		information_package_alreay_exists "$(basename "$pkg_filename")"
-		eval ${pkg}_PKG=\"$pkg_filename\"
-		export ${pkg?}_PKG
+	if [ -e "$package_filename" ] && [ $OVERWRITE_PACKAGES -ne 1 ]; then
+		information_package_already_exists "$(basename "$package_filename")"
+		eval ${package}_PKG=\"$package_filename\"
+		export ${package?}_PKG
 		return 0
 	fi
 
-	information_package_building "$(basename "$pkg_filename")"
+	information_package_building "$(basename "$package_filename")"
 	if [ "$DRY_RUN" -eq 1 ]; then
 		printf '\n'
-		eval ${pkg}_PKG=\"$pkg_filename\"
-		export ${pkg?}_PKG
+		eval ${package}_PKG=\"$package_filename\"
+		export ${package?}_PKG
 		return 0
 	fi
 
 	(
-		cd "$pkg_path"
-		tar $tar_options --file "$pkg_filename" *
+		cd "$package_path"
+		tar $tar_options --file "$package_filename" *
 	)
 
-	eval ${pkg}_PKG=\"$pkg_filename\"
-	export ${pkg?}_PKG
+	eval ${package}_PKG=\"$package_filename\"
+	export ${package?}_PKG
 
 	print_ok
 }
