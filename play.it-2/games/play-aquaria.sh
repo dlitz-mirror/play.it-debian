55268e4bde336ab1ae98ec9b14ac6a695cba27b9
vv221 <vv221@dotslashplay.it>
2021-04-23 23:23:32 +0200

    Aquaria — Syntax update

--- play.it-2/games/play-aquaria.sh
+++ play.it-2/games/play-aquaria.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 ###
 # Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2021, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -34,45 +35,47 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20210423.1
+script_version=20210423.2
 
 # Set game-specific variables
 
 GAME_ID='aquaria'
 GAME_NAME='Aquaria'
 
-ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_GOG_OLD'
+ARCHIVES_LIST='
+ARCHIVE_GOG
+ARCHIVE_GOG_OLD'
 
 ARCHIVE_GOG='gog_aquaria_2.0.0.5.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/aquaria'
 ARCHIVE_GOG_MD5='4235398debdf268f233881fade9e0530'
+ARCHIVE_GOG_TYPE='mojosetup'
 ARCHIVE_GOG_SIZE='240000'
 ARCHIVE_GOG_VERSION='1.1.3-gog2.0.0.5'
+ARCHIVE_GOG_URL='https://www.gog.com/game/aquaria'
 
 ARCHIVE_GOG_OLD='gog_aquaria_2.0.0.4.sh'
 ARCHIVE_GOG_OLD_MD5='1810de0d68028c6ec01d33181086180d'
+ARCHIVE_GOG_OLD_TYPT='mojosetup'
 ARCHIVE_GOG_OLD_SIZE='280000'
 ARCHIVE_GOG_OLD_VERSION='1.1.3-gog2.0.0.4'
 
-ARCHIVE_DOC1_PATH='data/noarch/docs'
-ARCHIVE_DOC1_FILES='./*'
+ARCHIVE_DOC0_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC0_DATA_FILES='*'
 
-ARCHIVE_DOC2_PATH='data/noarch/game'
-ARCHIVE_DOC2_FILES='./docs/* ./*.txt'
+ARCHIVE_DOC1_DATA_PATH='data/noarch/game'
+ARCHIVE_DOC1_DATA_FILES='docs/* *.txt'
 
 ARCHIVE_GAME_BIN_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN_FILES='./aquaria ./config ./*.xml'
+ARCHIVE_GAME_BIN_FILES='aquaria config *.xml'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='./aquaria.png ./data ./gfx ./_mods ./mus ./scripts ./sfx ./vox'
+ARCHIVE_GAME_DATA_FILES='aquaria.png data gfx _mods mus scripts sfx vox'
 
 CONFIG_FILES='./*.xml ./config/*'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='aquaria'
-APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
 APP_MAIN_ICON='aquaria.png'
-APP_MAIN_ICON_RES='128'
 
 PACKAGES_LIST='PKG_DATA PKG_BIN'
 
@@ -85,16 +88,15 @@ PKG_BIN_DEPS_ARCH="${PKG_DATA_ID} lib32-glibc lib32-gcc-libs lib32-libgl lib32-s
 
 # Load common functions
 
-target_version='2.3'
+target_version='2.12'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
 		if [ -e "$path/libplayit2.sh" ]; then
@@ -108,31 +110,26 @@ if [ -z "$PLAYIT_LIB2" ]; then
 	printf 'libplayit2.sh not found.\n'
 	exit 1
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
 
-PKG='PKG_BIN'
-organize_data 'GAME_BIN' "$PATH_GAME"
+# Clean up temporary files
 
-PKG='PKG_DATA'
-organize_data 'DOC1'      "$PATH_DOC"
-organize_data 'DOC2'      "$PATH_DOC"
-organize_data 'GAME_DATA' "$PATH_GAME"
-
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
-postinst_icons_linking 'APP_MAIN'
+icons_linking_postinst 'APP_MAIN'
 write_metadata 'PKG_DATA'
 write_metadata 'PKG_BIN'
 build_pkg
