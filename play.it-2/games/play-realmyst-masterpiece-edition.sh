0ecaef7f20a0112c96d371d466d4835d2f7b0590
vv221 <vv221@dotslashplay.it>
2021-04-28 23:15:24 +0200

    Restore support for Cyan games
    Restoring previously dropped support for:
    - Myst: Masterpiece Edition
    - RealMyst: Masterpiece Edition

---
+++ play.it-2/games/play-realmyst-masterpiece-edition.sh
@@ -0,0 +1,149 @@
+#!/bin/sh
+set -o errexit
+
+###
+# Copyright (c) 2015-2020, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# realMyst: Masterpiece Edition
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
+###
+
+script_version=20200813.2
+
+# Set game-specific variables
+
+GAME_ID='realmyst-masterpiece-edition'
+GAME_NAME='realMyst: Masterpiece Edition'
+
+ARCHIVES_LIST='
+ARCHIVE_GOG_0'
+
+ARCHIVE_GOG_0='setup_real_myst_masterpiece_edition_2.2_rev_10535_(64bit)_(23829).exe'
+ARCHIVE_GOG_0_MD5='fcb23e0256ab826e9a2ba9cad00d9a66'
+ARCHIVE_GOG_0_TYPE='innosetup'
+ARCHIVE_GOG_0_URL='https://www.gog.com/game/real_myst_masterpiece_edition'
+ARCHIVE_GOG_0_VERSION='2.2.10535-gog23829'
+ARCHIVE_GOG_0_SIZE='2800000'
+ARCHIVE_GOG_0_PART1='setup_real_myst_masterpiece_edition_2.2_rev_10535_(64bit)_(23829)-1.bin'
+ARCHIVE_GOG_0_PART1_MD5='038b24ec51a18b325574293d7f2d0ec2'
+ARCHIVE_GOG_0_PART1_TYPE='innosetup'
+
+ARCHIVE_GAME_BIN_PATH='.'
+ARCHIVE_GAME_BIN_FILES='realmyst.exe realmyst_data/mono'
+
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='realmyst_data'
+
+DATA_DIRS='./userdata'
+
+APP_MAIN_TYPE='wine'
+APP_MAIN_EXE='realmyst.exe'
+APP_MAIN_ICON='realmyst.exe'
+# Use a per-session dedicated file for logs
+APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
+# Store persistent user data outside of the game prefix
+APP_MAIN_PRERUN='# Store persistent user data outside of the game prefix
+user_data_path="$WINEPREFIX/drive_c/users/$USER/AppData/LocalLow/Cyan Worlds/realMyst"
+if [ ! -e "$user_data_path" ]; then
+	mkdir --parents "$(dirname "$user_data_path")"
+	mkdir --parents "$PATH_DATA/userdata"
+	ln --symbolic "$PATH_DATA/userdata" "$user_data_path"
+	init_prefix_dirs "$PATH_DATA" "$DATA_DIRS"
+fi'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='64'
+PKG_BIN_DEPS="$PKG_DATA_ID wine glx"
+
+# Load common functions
+
+target_version='2.12'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Extract icons
+
+PKG='PKG_BIN'
+icons_get_from_package 'APP_MAIN'
+icons_move_to 'PKG_DATA'
+
+# Clean up temporary files
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+launchers_write 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
