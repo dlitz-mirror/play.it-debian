14f32d7f884f270951eaba838581c8e3dc1c46a6
vv221 <vv221@dotslashplay.it>
2021-05-05 17:48:36 +0200

    Warhammer 40k: Dawn of War - Winter Assault demo — Add missing depencency on libGL.so.1

--- play.it-2/games/play-warhammer-40k-dawn-of-war-winter-assault-demo.sh
+++ play.it-2/games/play-warhammer-40k-dawn-of-war-winter-assault-demo.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20210423.1
+script_version=20210505.1
 
 # Set game-specific variables
 
@@ -80,7 +80,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID wine"
+PKG_BIN_DEPS="${PKG_DATA_ID} wine glx"
 
 # Load common functions
 
