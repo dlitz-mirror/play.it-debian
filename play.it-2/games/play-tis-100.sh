6bca4373c43601fd7c88cd7c9db6e7783c8bede5
vv221 <vv221@dotslashplay.it>
2021-05-07 18:51:50 +0200

    TIS-100 — Complete dependencies list

--- play.it-2/games/play-tis-100.sh
+++ play.it-2/games/play-tis-100.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20210507.8
+script_version=20210507.9
 
 # Set game-specific variables
 
@@ -72,7 +72,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr"
+PKG_BIN32_DEPS="${PKG_DATA_ID} glibc libstdc++ glx xcursor libxrandr gtk2 libX11.so.6 libgdk_pixbuf-2.0.so.0 libgobject-2.0.so.0 libglib-2.0.so.0"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
