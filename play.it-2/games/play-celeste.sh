be60eed24b5109c105c026caaf1a3e6c3e46a008
vv221 <vv221@dotslashplay.it>
2021-05-08 14:45:31 +0200

    Celeste — Syntax update

--- play.it-2/games/play-celeste.sh
+++ play.it-2/games/play-celeste.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine Le Gonidec <vv221@dotslashplay.it>
-# Copyright (c) 2018-2020, BetaRays
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2018-2021, BetaRays
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -35,18 +35,18 @@ set -o errexit
 # send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20201027.1
+script_version=20210508.1
 
 # Set game-specific variables
 
 GAME_ID='celeste'
 GAME_NAME='Celeste'
 
-ARCHIVE_ITCH='celeste-linux.zip'
-ARCHIVE_ITCH_URL='https://mattmakesgames.itch.io/celeste'
-ARCHIVE_ITCH_MD5='b27c983b95b2a2a35a272b7ecd94cbc2'
-ARCHIVE_ITCH_SIZE='1400000'
-ARCHIVE_ITCH_VERSION='1.3.1.2-itch'
+ARCHIVE_BASE_0='celeste-linux.zip'
+ARCHIVE_BASE_0_MD5='b27c983b95b2a2a35a272b7ecd94cbc2'
+ARCHIVE_BASE_0_SIZE='1400000'
+ARCHIVE_BASE_0_VERSION='1.3.1.2-itch'
+ARCHIVE_BASE_0_URL='https://mattmakesgames.itch.io/celeste'
 
 ARCHIVE_GAME_BIN32_PATH='.'
 ARCHIVE_GAME_BIN32_FILES='lib/libfmod*.so* lib/libmojoshader.so'
@@ -71,7 +71,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID mono sdl2 glx sdl2_image libxrandr"
+PKG_BIN32_DEPS="${PKG_DATA_ID} mono sdl2 glx sdl2_image libxrandr"
 PKG_BIN32_DEPS_DEB='libmono-2.0-1, libmono-posix4.0-cil, libmono-security4.0-cil, libmono-system-configuration4.0-cil, libmono-system-core4.0-cil, libmono-system-data4.0-cil, libmono-system4.0-cil, libmono-system-drawing4.0-cil, libmono-system-runtime-serialization4.0-cil, libmono-system-security4.0-cil, libmono-system-xml4.0-cil'
 
 PKG_BIN64_ARCH='64'
@@ -80,7 +80,7 @@ PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
 
 # Load common functions
 
-target_version='2.12'
+target_version='2.13'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	for path in \
@@ -91,8 +91,8 @@ if [ -z "$PLAYIT_LIB2" ]; then
 		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
-		if [ -e "$path/libplayit2.sh" ]; then
-			PLAYIT_LIB2="$path/libplayit2.sh"
+		if [ -e "${path}/libplayit2.sh" ]; then
+			PLAYIT_LIB2="${path}/libplayit2.sh"
 			break
 		fi
 	done
@@ -110,16 +110,19 @@ fi
 extract_data_from "$SOURCE_ARCHIVE"
 set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
 prepare_package_layout
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Get game icon
 
 PKG='PKG_DATA'
 icons_get_from_package 'APP_MAIN'
 
+# Clean up temporary files
+
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
+
 # Write launchers
 
-for PKG in PKG_BIN32 PKG_BIN64; do
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
 	launchers_write 'APP_MAIN'
 done
 
