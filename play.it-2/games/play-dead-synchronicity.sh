494bc73414952c12c423078a49fa30b34f7f5b94
vv221 <vv221@dotslashplay.it>
2021-07-12 23:51:43 +0200

    Dead Synchronicity — Syntax update

--- play.it-2/games/play-dead-synchronicity.sh
+++ play.it-2/games/play-dead-synchronicity.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2021, Mopi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,44 +31,36 @@ set -o errexit
 
 ###
 # Dead Synchronicity: Tomorrow Comes Today
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200918.1
+script_version=20210712.1
 
 # Set game-specific variables
 
 GAME_ID='dead-synchronicity'
 GAME_NAME='Dead Synchronicity: Tomorrow Comes Today'
 
-ARCHIVES_LIST='ARCHIVE_HUMBLE'
-
-ARCHIVE_HUMBLE='Dead_Synchronicity_1.0.7_Linux_Full_EN_FR_IT_DE_ES_KO_JA_Daedalic_noDRM.tar.gz'
-ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/dead-synchronicity-tomorrow-comes-today'
-ARCHIVE_HUMBLE_MD5='0aee9cc5d5c256f47ce61b313115a601'
-ARCHIVE_HUMBLE_SIZE='3200000'
-ARCHIVE_HUMBLE_VERSION='1.0.7-humble1'
+ARCHIVE_BASE_0='Dead_Synchronicity_1.0.7_Linux_Full_EN_FR_IT_DE_ES_KO_JA_Daedalic_noDRM.tar.gz'
+ARCHIVE_BASE_0_MD5='0aee9cc5d5c256f47ce61b313115a601'
+ARCHIVE_BASE_0_SIZE='3200000'
+ARCHIVE_BASE_0_VERSION='1.0.7-humble1'
+ARCHIVE_BASE_0_URL='https://www.humblebundle.com/store/dead-synchronicity-tomorrow-comes-today'
 
 ARCHIVE_GAME_BIN32_PATH='Dead Synchronicity'
-ARCHIVE_GAME_BIN32_FILES='./*.x86 ./*_Data/*/x86'
+ARCHIVE_GAME_BIN32_FILES='Dead?Synchronicity.x86 Dead?Synchronicity_Data/Mono/x86 Dead?Synchronicity_Data/Plugins/x86'
 
 ARCHIVE_GAME_BIN64_PATH='Dead Synchronicity'
-ARCHIVE_GAME_BIN64_FILES='./*.x86_64 ./*_Data/*/x86_64'
+ARCHIVE_GAME_BIN64_FILES='Dead?Synchronicity.x86_64 Dead?Synchronicity_Data/Mono/x86_64 Dead?Synchronicity_Data/Plugins/x86_64'
 
 ARCHIVE_GAME_DATA_PATH='Dead Synchronicity'
-ARCHIVE_GAME_DATA_FILES='./*_Data/level* ./*_Data/PlayerConnectionConfigFile ./*_Data/*.resS ./*_Data/*.assets ./*_Data/mainData ./*_Data/Mono/etc ./*_Data/Managed ./*_Data/Resources ./*_Data/version ./*_Data/versionNumber'
-
-DATA_DIRS='./logs'
+ARCHIVE_GAME_DATA_FILES='Dead?Synchronicity_Data'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE_BIN32='Dead Synchronicity.x86'
 APP_MAIN_EXE_BIN64='Dead Synchronicity.x86_64'
-# shellcheck disable=SC2016
-APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
-APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
-APP_MAIN_ICON='*_Data/Resources/UnityPlayer.png'
-APP_MAIN_ICON_RES='128'
+APP_MAIN_ICON='Dead Synchronicity_Data/Resources/UnityPlayer.png'
 
 PACKAGES_LIST='PKG_BIN32 PKG_BIN64 PKG_DATA'
 
@@ -75,27 +68,34 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glu xcursor libxrandr"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ xcursor libxrandr libGLU.so.1"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
+# Use a per-session dedicated file for logs
+
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Use a per-session dedicated file for logs
+mkdir --parents logs
+APP_OPTIONS="${APP_OPTIONS} -logFile ./logs/$(date +%F-%R).log"'
+
 # Load common functions
 
-target_version='2.5'
+target_version='2.13'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
-		if [ -e "$path/libplayit2.sh" ]; then
-			PLAYIT_LIB2="$path/libplayit2.sh"
+		if [ -e "${path}/libplayit2.sh" ]; then
+			PLAYIT_LIB2="${path}/libplayit2.sh"
 			break
 		fi
 	done
@@ -105,30 +105,32 @@ if [ -z "$PLAYIT_LIB2" ]; then
 	printf 'libplayit2.sh not found.\n'
 	exit 1
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
 
-for PKG in $PACKAGES_LIST; do
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
+# Get game icon
 
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+PKG='PKG_DATA'
+icons_get_from_package 'APP_MAIN'
+
+# Delete temporary files
+
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
 
 # Write launchers
 
 for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
-	write_launcher 'APP_MAIN'
+	launchers_write 'APP_MAIN'
 done
 
 # Build package
 
-postinst_icons_linking 'APP_MAIN'
-write_metadata 'PKG_DATA'
-write_metadata 'PKG_BIN32' 'PKG_BIN64'
+write_metadata
 build_pkg
 
 # Clean up
@@ -137,10 +139,6 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-printf '\n'
-printf '32-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN32'
-printf '64-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN64'
+print_instructions
 
 exit 0
