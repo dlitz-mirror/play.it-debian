a8e103e846fb0df0e230c37c398fc51da24ddfbc
vv221 <vv221@dotslashplay.it>
2021-07-16 15:48:43 +0200

    Syberia — Syntax update

--- play.it-2/games/play-syberia.sh
+++ play.it-2/games/play-syberia.sh
@@ -1,10 +1,10 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
-# Copyright (c) 2016-2020, Mopi
-# Copyright (c) 2020, Igor Telmenko
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2021, Mopi
+# Copyright (c) 2020-2021, Igor Telmenko
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,49 +32,52 @@ set -o errexit
 
 ###
 # Syberia
-# build native Linux packages from the original installers
-# send your bug reports to mopi@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20200104.4
+script_version=20210716.2
 
 # Set game-specific variables
 
 GAME_ID='syberia'
 GAME_NAME='Syberia'
 
-ARCHIVES_LIST='ARCHIVE_GOG_RU ARCHIVE_GOG_EN ARCHIVE_GOG_EN_OLD ARCHIVE_GOG_FR ARCHIVE_GOG_FR_OLD'
+ARCHIVE_BASE_RU_1='setup_syberia_russian_1.0.0_hotfix3_(18946).exe'
+ARCHIVE_BASE_RU_1_MD5='cdf5ac1869d57d139495a20102d5ffb4'
+ARCHIVE_BASE_RU_1_TYPE='innosetup'
+ARCHIVE_BASE_RU_1_SIZE='1600000'
+ARCHIVE_BASE_RU_1_VERSION='1.0.0.3-gog18946'
+ARCHIVE_BASE_RU_1_URL='https://www.gog.com/game/syberia'
 
-ARCHIVE_GOG_RU='setup_syberia_russian_1.0.0_hotfix3_(18946).exe'
-ARCHIVE_GOG_RU_URL='https://www.gog.com/game/syberia'
-ARCHIVE_GOG_RU_MD5='cdf5ac1869d57d139495a20102d5ffb4'
-ARCHIVE_GOG_RU_SIZE='1600000'
-ARCHIVE_GOG_RU_VERSION='1.0.0.3-gog18946'
+ARCHIVE_BASE_EN_1='setup_syberia_1.0.0_hotfix3_(18946).exe'
+ARCHIVE_BASE_EN_1_MD5='53d91df35a154584812d31b9ee353cb8'
+ARCHIVE_BASE_EN_1_TYPE='innosetup'
+ARCHIVE_BASE_EN_1_SIZE='1600000'
+ARCHIVE_BASE_EN_1_VERSION='1.0.0.3-gog18946'
+ARCHIVE_BASE_EN_1_URL='https://www.gog.com/game/syberia'
 
-ARCHIVE_GOG_EN='setup_syberia_1.0.0_hotfix3_(18946).exe'
-ARCHIVE_GOG_EN_URL='https://www.gog.com/game/syberia'
-ARCHIVE_GOG_EN_MD5='53d91df35a154584812d31b9ee353cb8'
-ARCHIVE_GOG_EN_SIZE='1600000'
-ARCHIVE_GOG_EN_VERSION='1.0.0.3-gog18946'
+ARCHIVE_BASE_FR_1='setup_syberia_french_1.0.0_hotfix3_(18946).exe'
+ARCHIVE_BASE_FR_1_MD5='41881248eefd53929bbaa97c1905a7fe'
+ARCHIVE_BASE_FR_1_TYPE='innosetup'
+ARCHIVE_BASE_FR_1_SIZE='1600000'
+ARCHIVE_BASE_FR_1_VERSION='1.0.0.3-gog18946'
+ARCHIVE_BASE_FR_1_URL='https://www.gog.com/game/syberia'
 
-ARCHIVE_GOG_EN_OLD='setup_syberia_1.0.0_hotfix2_(17897).exe'
-ARCHIVE_GOG_EN_OLD_MD5='d52b7a776df7659d7fda9995715468a0'
-ARCHIVE_GOG_EN_OLD_SIZE='1600000'
-ARCHIVE_GOG_EN_OLD_VERSION='1.0.0.2-gog17897'
+ARCHIVE_BASE_EN_0='setup_syberia_1.0.0_hotfix2_(17897).exe'
+ARCHIVE_BASE_EN_0_MD5='d52b7a776df7659d7fda9995715468a0'
+ARCHIVE_BASE_EN_0_TYPE='innosetup'
+ARCHIVE_BASE_EN_0_SIZE='1600000'
+ARCHIVE_BASE_EN_0_VERSION='1.0.0.2-gog17897'
 
-ARCHIVE_GOG_FR='setup_syberia_french_1.0.0_hotfix3_(18946).exe'
-ARCHIVE_GOG_FR_URL='https://www.gog.com/game/syberia'
-ARCHIVE_GOG_FR_MD5='41881248eefd53929bbaa97c1905a7fe'
-ARCHIVE_GOG_FR_SIZE='1600000'
-ARCHIVE_GOG_FR_VERSION='1.0.0.3-gog18946'
-
-ARCHIVE_GOG_FR_OLD='setup_syberia_french_1.0.0_hotfix2_(17897).exe'
-ARCHIVE_GOG_FR_OLD_MD5='d8a956a47c1b186a4364eff56c8cecb6'
-ARCHIVE_GOG_FR_OLD_SIZE='1600000'
-ARCHIVE_GOG_FR_OLD_VERSION='1.0.0.2-gog17897'
+ARCHIVE_BASE_FR_0='setup_syberia_french_1.0.0_hotfix2_(17897).exe'
+ARCHIVE_BASE_FR_0_MD5='d8a956a47c1b186a4364eff56c8cecb6'
+ARCHIVE_BASE_FR_0_TYPE='innosetup'
+ARCHIVE_BASE_FR_0_SIZE='1600000'
+ARCHIVE_BASE_FR_0_VERSION='1.0.0.2-gog17897'
 
 ARCHIVE_DOC_L10N_PATH='app'
-ARCHIVE_DOC_L10N_FILES='./*.pdf ./*.txt'
+ARCHIVE_DOC_L10N_FILES='*.pdf *.txt'
 
 ARCHIVE_GAME_BIN_PATH='app'
 ARCHIVE_GAME_BIN_FILES='dlls *.dll *.exe *.ini */*.ini'
@@ -87,68 +90,76 @@ ARCHIVE_GAME_DATA_FILES='cmo data textures video'
 
 CONFIG_FILES='./*.ini ./*/*.ini'
 
-APP_REGEDIT="$GAME_ID.reg"
-
 APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='game.exe'
 APP_MAIN_ICON='syberia.exe'
 
-PACKAGES_LIST='PKG_BIN PKG_L10N PKG_DATA'
-
-PKG_L10N_ID="${GAME_ID}-l10n"
-PKG_L10N_ID_GOG_EN="${PKG_L10N_ID}-en"
-PKG_L10N_ID_GOG_FR="${PKG_L10N_ID}-fr"
-PKG_L10N_ID_GOG_RU="${PKG_L10N_ID}-ru"
-PKG_L10N_PROVIDE="$PKG_L10N_ID"
-PKG_L10N_DESCRIPTION_GOG_EN='English localization'
-PKG_L10N_DESCRIPTION_GOG_FR='French localization'
-PKG_L10N_DESCRIPTION_GOG_RU='Russian localization'
+PACKAGES_LIST='PKG_BIN PKG_DATA'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ID="$GAME_ID"
 PKG_BIN_ARCH='32'
-PKG_BIN_ID_GOG_EN="${PKG_BIN_ID}-en"
-PKG_BIN_ID_GOG_FR="${PKG_BIN_ID}-fr"
-PKG_BIN_ID_GOG_RU="${PKG_BIN_ID}-ru"
 PKG_BIN_PROVIDE="$PKG_BIN_ID"
-PKG_BIN_DEPS="$PKG_L10N_ID $PKG_DATA_ID wine"
+PKG_BIN_DEPS="$PKG_DATA_ID wine"
+
+# Localization
+
+PACKAGES_LIST="PKG_L10N $PACKAGES_LIST"
+PKG_L10N_ID="${GAME_ID}-l10n"
+PKG_L10N_PROVIDE="$PKG_L10N_ID"
+PKG_BIN_DEPS="$PKG_BIN_DEPS $PKG_L10N_ID"
+
+## English
+
+PKG_BIN_ID_EN="${PKG_BIN_ID}-en"
+PKG_L10N_ID_EN="${PKG_L10N_ID}-en"
+PKG_L10N_DESCRIPTION_EN='English localization'
+
+## French
+
+PKG_BIN_ID_FR="${PKG_BIN_ID}-fr"
+PKG_L10N_ID_FR="${PKG_L10N_ID}-fr"
+PKG_L10N_DESCRIPTION_FR='French localization'
+
+## Russian
+
+PKG_BIN_ID_RU="${PKG_BIN_ID}-ru"
+PKG_L10N_ID_RU="${PKG_L10N_ID}-ru"
+PKG_L10N_DESCRIPTION_RU='Russian localization'
 
 # Load common functions
 
-target_version='2.9'
+target_version='2.13'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	for path in\
-		'./'\
-		"$XDG_DATA_HOME/play.it/"\
-		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
-		'/usr/local/share/games/play.it/'\
-		'/usr/local/share/play.it/'\
-		'/usr/share/games/play.it/'\
-		'/usr/share/play.it/'
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
+		'/usr/share/play.it'
 	do
-		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
-			PLAYIT_LIB2="$path/libplayit2.sh"
+		if [ -e "${path}/libplayit2.sh" ]; then
+			PLAYIT_LIB2="${path}/libplayit2.sh"
 			break
 		fi
 	done
-	if [ -z "$PLAYIT_LIB2" ]; then
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Extract icons
 
@@ -156,17 +167,27 @@ PKG='PKG_BIN'
 icons_get_from_package 'APP_MAIN'
 icons_move_to 'PKG_DATA'
 
+# Delete temporary files
+
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
+
 # Create player.ini file if one is not already provided
 
-if [ ! -e "${PKG_BIN_PATH}${PATH_GAME}/player.ini" ]; then
-	cat > "${PKG_BIN_PATH}${PATH_GAME}/player.ini" <<- EOF
+PKG='PKG_BIN'
+config_file="$(package_get_path "$PKG")${PATH_GAME}/player.ini"
+if [ ! -e "$config_file" ]; then
+	cat > "$config_file" <<- EOF
 	800 600 16 0 BaseCMO.cmo
 	EOF
 fi
 
-# Add workaround for crash before reaching game menu
+# Work around a crash on nvidia drivers
+# cf. https://bugs.winehq.org/show_bug.cgi?id=43199
+#     https://bugs.winehq.org/show_bug.cgi?id=44009
 
-cat > "${PKG_BIN_PATH}${PATH_GAME}/$APP_REGEDIT" << 'EOF'
+PKG='PKG_BIN'
+registry_file="$(package_get_path "$PKG")${PATH_GAME}/${APP_REGEDIT:=no-xvidmode.reg}"
+cat > "$registry_file" << 'EOF'
 Windows Registry Editor Version 5.00
 
 [HKEY_CURRENT_USER\Software\Wine\X11 Driver]
@@ -176,7 +197,7 @@ EOF
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
