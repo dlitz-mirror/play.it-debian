f8d5e7c8da1110dc8f07c942b027a08d63f829f4
vv221 <vv221@dotslashplay.it>
2021-07-24 00:31:40 +0200

    New function printing the list of applications for the current game
    list application identifiers for the current game
    USAGE: applications_list
    RETURN: a space-separated list of application identifiers

---
+++ play.it-2/src/20_applications.sh
@@ -0,0 +1,29 @@
+# list application identifiers for the current game
+# USAGE: applications_list
+# RETURN: a space-separated list of application identifiers
+applications_list() {
+	# If APPLICATIONS_LIST is set, return it instead of trying to guess the list
+	if [ -n "$APPLICATIONS_LIST" ]; then
+		printf '%s' "$APPLICATIONS_LIST"
+		return 0
+	fi
+
+	# If APPLICATIONS_LIST is not set, try to guess a list by parsing the game script
+	# shellcheck disable=SC2039
+	local game_script
+	game_script="$0"
+
+	# Try to generate a list from the following variables:
+	# - APP_xxx_EXE
+	# - APP_xxx_SCUMMID
+	# - APP_xxx_RESIDUALID
+	# shellcheck disable=SC2039
+	local sed_expression application_id
+	sed_expression='s/^\(APP_[0-9A-Z]\+\)_\(EXE\|SCUMMID\|RESIDUALID\)\(_[0-9A-Z]\+\)\?=.*/\1/p'
+	while read -r application_id; do
+		printf '%s ' "$application_id"
+	done <<- EOF
+	$(sed --silent --expression="$sed_expression" "$game_script")
+	EOF
+}
+
