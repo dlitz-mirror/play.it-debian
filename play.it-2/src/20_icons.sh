dbbd61a6dae7fde4b22b8f1f4c56179d2f2583b3
vv221 <vv221@dotslashplay.it>
2021-08-01 18:29:41 +0200

    New function fetching a custom options string to pass to wrestool
    This options string can be set from game scripts using a new variable:
    xxx_WRESTOOL_OPTIONS
    ("xxx" being the icon identifier, usually "APP_MAIN_ICON")
    
    print the wrestool options string for the given .exe icon
    USAGE: icon_wrestool_options $icon
    RETURN: the options string to pass to wrestool

--- play.it-2/src/20_icons.sh
+++ play.it-2/src/20_icons.sh
@@ -15,3 +15,36 @@ icon_path() {
 	printf '%s' "$icon_path"
 }
 
+# print the wrestool options string for the given .exe icon
+# USAGE: icon_wrestool_options $icon
+# RETURN: the options string to pass to wrestool
+icon_wrestool_options() {
+	# shellcheck disable=SC2039
+	local icon
+	icon="$1"
+
+	# Check that the given icon is a .exe file
+	if ! icon_path "$icon" | grep --quiet '\.exe$'; then
+		error_icon_unexpected_type "$icon" '*.exe'
+		return 1
+	fi
+
+	# Fetch the custom options string
+	# shellcheck disable=SC2039
+	wrestool_options=$(get_value "${icon}_WRESTOOL_OPTIONS")
+
+	# If no custom value is set, falls back to defaults
+	: "${wrestool_options:=--type=14}"
+
+	###
+	# TODO
+	# This should be deprecated
+	###
+	# Check for an explicit wrestool id
+	if [ -n "$(get_value "${icon}_ID")" ]; then
+		wrestool_options="$wrestool_options --name=$(get_value "${icon}_ID")"
+	fi
+
+	printf '%s' "$wrestool_options"
+}
+
