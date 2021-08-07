5dbda3d738a0f6c641d76dfc4b55f3a36dde7a29
vv221 <vv221@dotslashplay.it>
2021-08-07 20:50:22 +0200

    Don't Starve — Syntax update

--- play.it-2/games/play-dont-starve.sh
+++ play.it-2/games/play-dont-starve.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 ###
-# Copyright (c) 2015-2020, Antoine "vv221/vv222" Le Gonidec
-# Copyright (c) 2016-2020, Mopi
-# Copyright (c) 2018-2020, BetaRays
+# Copyright (c) 2015-2021, Antoine Le Gonidec <vv221@dotslashplay.it>
+# Copyright (c) 2016-2021, Mopi
+# Copyright (c) 2018-2021, BetaRays
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -31,54 +31,56 @@ set -o errexit
 ###
 
 ###
-# Don’t Starve
+# Don't Starve
 # build native packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# send your bug reports to contact@dotslashplay.it
 ###
 
-script_version=20190706.1
+script_version=20210807.3
 
 # Set game-specific variables
 
 GAME_ID='dont-starve'
 GAME_NAME='Donʼt Starve'
 
-ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_GOG_MULTIARCH_OLD2 ARCHIVE_GOG_MULTIARCH_OLD1 ARCHIVE_GOG_MULTIARCH_OLD0'
+ARCHIVE_BASE_0='don_t_starve_en_222215_22450.sh'
+ARCHIVE_BASE_0_URL='https://www.gog.com/game/dont_starve'
+ARCHIVE_BASE_0_MD5='611cd70afd9b9feb3aca4d1eaf9ebbda'
+ARCHIVE_BASE_0_TYPE='mojosetup'
+ARCHIVE_BASE_0_SIZE='760000'
+ARCHIVE_BASE_0_VERSION='276758-gog22450'
 
-ARCHIVE_GOG='don_t_starve_en_222215_22450.sh'
-ARCHIVE_GOG_URL='https://www.gog.com/game/dont_starve'
-ARCHIVE_GOG_MD5='611cd70afd9b9feb3aca4d1eaf9ebbda'
-ARCHIVE_GOG_TYPE='mojosetup'
-ARCHIVE_GOG_SIZE='760000'
-ARCHIVE_GOG_VERSION='276758-gog22450'
+ARCHIVE_BASE_MULTIARCH_2='don_t_starve_en_20171215_17629.sh'
+ARCHIVE_BASE_MULTIARCH_2_MD5='f7dda3b3bdb15ac62acb212a89b24623'
+ARCHIVE_BASE_MULTIARCH_2_TYPE='mojosetup'
+ARCHIVE_BASE_MULTIARCH_2_SIZE='670000'
+ARCHIVE_BASE_MULTIARCH_2_VERSION='246924-gog17629'
 
-ARCHIVE_GOG_MULTIARCH_OLD2='don_t_starve_en_20171215_17629.sh'
-ARCHIVE_GOG_MULTIARCH_OLD2_MD5='f7dda3b3bdb15ac62acb212a89b24623'
-ARCHIVE_GOG_MULTIARCH_OLD2_TYPE='mojosetup'
-ARCHIVE_GOG_MULTIARCH_OLD2_SIZE='670000'
-ARCHIVE_GOG_MULTIARCH_OLD2_VERSION='246924-gog17629'
+ARCHIVE_BASE_MULTIARCH_1='gog_don_t_starve_2.7.0.9.sh'
+ARCHIVE_BASE_MULTIARCH_1_MD5='01d7496de1c5a28ffc82172e89dd9cd6'
+ARCHIVE_BASE_MULTIARCH_1_SIZE='660000'
+ARCHIVE_BASE_MULTIARCH_1_VERSION='222215-gog2.7.0.9'
 
-ARCHIVE_GOG_MULTIARCH_OLD1='gog_don_t_starve_2.7.0.9.sh'
-ARCHIVE_GOG_MULTIARCH_OLD1_MD5='01d7496de1c5a28ffc82172e89dd9cd6'
-ARCHIVE_GOG_MULTIARCH_OLD1_SIZE='660000'
-ARCHIVE_GOG_MULTIARCH_OLD1_VERSION='222215-gog2.7.0.9'
-
-ARCHIVE_GOG_MULTIARCH_OLD0='gog_don_t_starve_2.6.0.8.sh'
-ARCHIVE_GOG_MULTIARCH_OLD0_MD5='2b0d363bea53654c0267ae424de7130a'
-ARCHIVE_GOG_MULTIARCH_OLD0_SIZE='650000'
-ARCHIVE_GOG_MULTIARCH_OLD0_VERSION='198251-gog2.6.0.8'
-
-ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC_DATA_FILES='*'
-
-ARCHIVE_GAME_BIN32_PATH='data/noarch/game/dontstarve32'
-ARCHIVE_GAME_BIN32_FILES='*.json bin/dontstarve bin/lib32/libfmodevent.so bin/lib32/libfmodevent-4.44.07.so bin/lib32/libfmodex.so bin/lib32/libfmodex-4.44.07.so bin/lib32/libSDL2*'
+ARCHIVE_BASE_MULTIARCH_0='gog_don_t_starve_2.6.0.8.sh'
+ARCHIVE_BASE_MULTIARCH_0_MD5='2b0d363bea53654c0267ae424de7130a'
+ARCHIVE_BASE_MULTIARCH_0_SIZE='650000'
+ARCHIVE_BASE_MULTIARCH_0_VERSION='198251-gog2.6.0.8'
 
 ARCHIVE_GAME_BIN64_PATH='data/noarch/game/dontstarve64'
-ARCHIVE_GAME_BIN64_FILES='*.json bin/dontstarve bin/lib64/libfmodevent64.so bin/lib64/libfmodevent64-4.44.07.so bin/lib64/libfmodex64.so bin/lib64/libfmodex64-4.44.07.so bin/lib64/libSDL2*'
+ARCHIVE_GAME_BIN64_FILES='
+*.json
+bin/dontstarve
+bin/lib64/libfmodevent64.so
+bin/lib64/libfmodevent64-4.44.07.so
+bin/lib64/libfmodex64.so
+bin/lib64/libfmodex64-4.44.07.so
+bin/lib64/libSDL2*'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game/dontstarve64'
-ARCHIVE_GAME_DATA_FILES='data mods dontstarve.xpm'
+ARCHIVE_GAME_DATA_FILES='
+data
+mods
+dontstarve.xpm'
 
 DATA_DIRS='./mods'
 
@@ -87,8 +89,6 @@ APP_MAIN_EXE='bin/dontstarve'
 APP_MAIN_ICON='dontstarve.xpm'
 
 PACKAGES_LIST='PKG_BIN64 PKG_DATA'
-# Keep compatibility with old archives
-PACKAGES_LIST_GOG_MULTIARCH='PKG_BIN32 PKG_BIN64 PKG_DATA'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
@@ -96,25 +96,46 @@ PKG_DATA_DESCRIPTION='data'
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_DATA_ID glibc libstdc++ libcurl-gnutls glx sdl2"
 
+# Run the game binary from its parent directory
+
+APP_MAIN_PRERUN="$APP_MAIN_PRERUN"'
+
+# Run the game binary from its parent directory
+cd "$(dirname "$APP_EXE")"
+APP_EXE=$(basename "$APP_EXE")'
+
+# Keep compatibility with old archives
+
+ARCHIVE_GAME_BIN32_PATH='data/noarch/game/dontstarve32'
+ARCHIVE_GAME_BIN32_FILES='
+*.json
+bin/dontstarve
+bin/lib32/libfmodevent.so
+bin/lib32/libfmodevent-4.44.07.so
+bin/lib32/libfmodex.so
+bin/lib32/libfmodex-4.44.07.so
+bin/lib32/libSDL2*'
+
 PKG_BIN32_ARCH='32'
 PKG_BIN32_DEPS="$PKG_BIN64_DEPS"
 
+PACKAGES_LIST_MULTIARCH='PKG_BIN32 PKG_BIN64 PKG_DATA'
+
 # Load common functions
 
-target_version='2.11'
+target_version='2.13'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
-	for path in\
-		"$PWD"\
-		"$XDG_DATA_HOME/play.it"\
-		'/usr/local/share/games/play.it'\
-		'/usr/local/share/play.it'\
-		'/usr/share/games/play.it'\
+	for path in \
+		"$PWD" \
+		"${XDG_DATA_HOME:="$HOME/.local/share"}/play.it" \
+		'/usr/local/share/games/play.it' \
+		'/usr/local/share/play.it' \
+		'/usr/share/games/play.it' \
 		'/usr/share/play.it'
 	do
-		if [ -e "$path/libplayit2.sh" ]; then
-			PLAYIT_LIB2="$path/libplayit2.sh"
+		if [ -e "${path}/libplayit2.sh" ]; then
+			PLAYIT_LIB2="${path}/libplayit2.sh"
 			break
 		fi
 	done
@@ -137,9 +158,12 @@ set_temp_directories $PACKAGES_LIST
 
 extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
+
+# Delete temporary files
+
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
-# Get icon
+# Include game icon
 
 PKG='PKG_DATA'
 # Work around icon_extract_png_from_file not supporting .xpm files yet
@@ -151,29 +175,11 @@ rm --force "${PKG_DATA_PATH}${PATH_GAME}/${APP_MAIN_ICON%.xpm}.bmp"
 
 # Write launchers
 
-PKG='PKG_BIN64'
-launchers_write 'APP_MAIN'
-if [ -n "$PKG_BIN32_PATH" ] && [ -d "$PKG_BIN32_PATH" ]; then
-	PKG='PKG_BIN32'
-	launchers_write 'APP_MAIN'
-fi
-
-# Set working directory to the directory containing the game binary before running it
-
-# shellcheck disable=SC2016
-pattern='s|^cd "$PATH_PREFIX"$|cd "$PATH_PREFIX/${APP_EXE%/*}"|'
-# shellcheck disable=SC2016
-pattern="$pattern"';s|^"\./$APP_EXE"|"./${APP_EXE##*/}"|'
-file="${PKG_BIN64_PATH}${PATH_BIN}/$GAME_ID"
-if [ $DRY_RUN -eq 0 ]; then
-	sed --in-place "$pattern" "$file"
-fi
-if [ -n "$PKG_BIN32_PATH" ] && [ -d "$PKG_BIN32_PATH" ]; then
-	file="${PKG_BIN32_PATH}${PATH_BIN}/$GAME_ID"
-	if [ $DRY_RUN -eq 0 ]; then
-		sed --in-place "$pattern" "$file"
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	if packages_get_list | grep --quiet "$PKG"; then
+		launchers_write 'APP_MAIN'
 	fi
-fi
+done
 
 # Build package
 
