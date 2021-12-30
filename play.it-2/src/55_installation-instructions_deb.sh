7010ca7d1edfe6d6f24979d6dced6c4a765d924a
Antoine Le Gonidec <vv221@dotslashplay.it>
2021-12-30 11:41:08 +0100

    Debian — Drop support for apt < 1.1 when displaying packages installation instructions

--- play.it-2/src/55_installation-instructions_deb.sh
+++ play.it-2/src/55_installation-instructions_deb.sh
@@ -1,55 +1,21 @@
 # print installation instructions for Debian
 # USAGE: print_instructions_deb $pkg[…]
-# CALLS: print_instructions_deb_apt print_instructions_deb_dpkg
 print_instructions_deb() {
-	if command -v apt >/dev/null 2>&1; then
-		local apt_version
-		apt_version=$(LANG=C apt --version 2>/dev/null | \
-			grep --extended-regexp --only-matching '[0-9]+(\.[0-9]+)+')
-		if version_is_at_least '1.1' "$apt_version"; then
-			print_instructions_deb_apt "$@"
-		else
-			print_instructions_deb_dpkg "$@"
-		fi
-	else
-		print_instructions_deb_dpkg "$@"
-	fi
-}
-
-# print installation instructions for Debian with apt
-# USAGE: print_instructions_deb_apt $pkg[…]
-# CALLS: print_instructions_deb_common
-# CALLED BY: print_instructions_deb
-print_instructions_deb_apt() {
 	printf 'apt install'
-	print_instructions_deb_common "$@"
-}
-
-# print installation instructions for Debian with dpkg + apt-get
-# USAGE: print_instructions_deb_dpkg $pkg[…]
-# CALLS: print_instructions_deb_common
-# CALLED BY: print_instructions_deb
-print_instructions_deb_dpkg() {
-	printf 'dpkg -i'
-	print_instructions_deb_common "$@"
-	printf 'apt-get install -f\n'
-}
-
-# print installation instructions for Debian (common part)
-# USAGE: print_instructions_deb_common $pkg[…]
-# CALLED BY: print_instructions_deb_apt print_instructions_deb_dpkg
-print_instructions_deb_common() {
-	local pkg_path
-	local str_format
-
-	for pkg in "$@"; do
-		pkg_path=$(realpath "$(get_value "${pkg}_PKG")")
-		if [ -z "${pkg_path##* *}" ]; then
-			str_format=' "%s"'
+	local package
+	for package in "$@"; do
+		local package_path package_name package_output
+		package_path=$(package_get_path "$package")
+		package_name=$(basename "$package_path")
+		package_output=$(realpath "${OPTION_OUTPUT_DIR}/${package_name}.deb")
+		local string_format
+		if printf '%s' "$package_output" | grep --quiet ' '; then
+			string_format=' "%s"'
 		else
-			str_format=' %s'
+			string_format=' %s'
 		fi
-		printf "$str_format" "$pkg_path"
+		# shellcheck disable=SC2059
+		printf "$string_format" "$package_output"
 	done
 	printf '\n'
 }
