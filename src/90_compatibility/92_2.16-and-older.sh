0eaf97cfd77532506c28fd7b3be94c3e95c8039c
Antoine Le Gonidec <vv221@dotslashplay.it>
2022-07-07 00:10:42 +0200

    Provide icons inclusion using a single function
    A single function is now available to game scripts, for icons inclusion: icons_inclusion
    
    USAGE: icons_inclusion $application[…]
    
    This function is the one that should be called from game scripts,
    it can take several applications as its arguments,
    and default to handle all applications if none are explicitely given.
    
    It relies on APP_xxx_ICONS_LIST / APP_xxx_ICON, like the previous
    functions. It looks for the icon files under the following path:
    ${PLAYIT_WORKDIR}/gamedata/${CONTENT_PATH_DEFAULT}
    
    Compatibility wrappers are available for the following deprecated functions:
    - icons_get_from_package
    - icons_get_from_workdir

---
+++ src/90_compatibility/92_2.16-and-older.sh
@@ -0,0 +1,60 @@
+# Keep compatibility with 2.16 and older
+
+icons_get_from_package() {
+	local package package_path
+	package=$(package_get_current)
+	package_path=$(package_get_path "$package")
+	icon_source_directory="${package_path}/${PATH_GAME}"
+	icons_get_from_legacy_path "$icon_source_directory" "$@"
+}
+
+icons_get_from_workdir() {
+	local icon_source_directory
+	icon_source_directory="${PLAYIT_WORKDIR}/gamedata"
+	icons_get_from_legacy_path "$icon_source_directory" "$@"
+}
+
+icons_get_from_legacy_path() {
+	local icon_source_directory_legacy
+	icon_source_directory_legacy="$1"
+	shift 1
+	if [ -z "$CONTENT_PATH_DEFAULT" ]; then
+		local CONTENT_PATH_DEFAULT
+		CONTENT_PATH_DEFAULT='.'
+	fi
+	local application
+	for application in "$@"; do
+		assert_not_empty 'application' 'icons_get_from_path'
+		local application_icons_list
+		application_icons_list=$(application_icons_list "$application")
+		if [ -z "$application_icons_list" ]; then
+			continue
+		fi
+		local icon
+		for icon in $application_icons_list; do
+			assert_not_empty 'icon' 'icons_get_from_path'
+			local icon_path icon_full_path_legacy content_path icon_source_directory_new icon_full_path_new
+			icon_path=$(icon_path "$icon")
+			# Compute icon legacy path
+			icon_full_path_legacy="${icon_source_directory_legacy}/${icon_path}"
+			# Compute icon expected path for new function
+			content_path=$(content_path_default)
+			icon_source_directory_new="${PLAYIT_WORKDIR}/gamedata/${content_path}"
+			icon_full_path_new="${icon_source_directory_new}/${icon_path}"
+			# Set compatibility link
+			icon_full_path_legacy_canonical=$(realpath --canonicalize-missing "$icon_full_path_legacy")
+			icon_full_path_new_canonical=$(realpath --canonicalize-missing "$icon_full_path_new")
+			if [ "$icon_full_path_legacy_canonical" != "$icon_full_path_new_canonical" ]; then
+				mkdir --parents "$(dirname "$icon_full_path_new")"
+				ln --symbolic "$icon_full_path_legacy" "$icon_full_path_new"
+			fi
+			# Call new function
+			icons_inclusion_single_icon "$application" "$icon"
+			# Remove compatibility link
+			if [ "$icon_full_path_legacy_canonical" != "$icon_full_path_new_canonical" ]; then
+				rm "$icon_full_path_new"
+				rmdir --parents --ignore-fail-on-non-empty "$(dirname "$icon_full_path_new")"
+			fi
+		done
+	done
+}
