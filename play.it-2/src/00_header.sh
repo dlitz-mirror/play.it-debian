3f198dfa51f308971174230c4f53283d5180581f
Antoine Le Gonidec <vv221@dotslashplay.it>
2022-07-28 19:25:44 +0200

    Bump library version and revision

--- play.it-2/src/00_header.sh
+++ play.it-2/src/00_header.sh
@@ -41,7 +41,7 @@
 # send your bug reports to contact@dotslashplay.it
 ###
 
-LIBRARY_VERSION=2.16.1
+LIBRARY_VERSION=2.16.2
 # shellcheck disable=SC2034
-library_revision=20220713.2
+library_revision=20220728.2
 
