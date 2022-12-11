21a712d96b6c4afd55cf8014e69ea33fb4228c6e
Antoine Le Gonidec <vv221@dotslashplay.it>
2022-12-11 19:35:06 +0100

    Provide a new variable manipulation helper "variable_is_empty"
    Usage: variable_is_empty $variable_name
    Return: 0 if the variable is unset or empty, 1 if it is set to a non empty value

--- src/10_common/10_variables-manipulation.sh
+++ src/10_common/10_variables-manipulation.sh
@@ -131,3 +131,18 @@ variable_is_set() {
 	variable_name="$1"
 	set | grep --quiet --regexp="^${variable_name}="
 }
+
+# Check if the given varible is unset or set to an empty value.
+# USAGE: variable_is_empty $variable_name
+# RETURN: 0 if the variable is unset or empty, 1 if it is set to a non empty value
+variable_is_empty() {
+	local variable_name
+	variable_name="$1"
+	if ! variable_is_set "$variable_name"; then
+		return 0
+	fi
+
+	local variable_value
+	variable_value=$(get_value "$variable_name")
+	test -z "$variable_value"
+}
