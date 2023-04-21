b1871730700d7ac53a89b2e4ff3f837ef606caae
Antoine Le Gonidec <vv221@dotslashplay.it>
2023-04-21 15:41:03 +0200

    Rework the generation of Mono launchers
    Support for APP_xxx_MONO_OPTIONS is dropped, as no game script is using it.

--- src/50_engine_mono/10_launchers.sh
+++ src/50_engine_mono/10_launchers.sh
@@ -1,4 +1,4 @@
-# Mono - Print the content of the launcher script
+# Mono launcher - Print the script content
 # USAGE: mono_launcher $application
 mono_launcher() {
 	local application
@@ -9,19 +9,20 @@ mono_launcher() {
 	case "$prefix_type" in
 		('symlinks')
 			launcher_headers
-			mono_launcher_application_variables "$application"
-			launcher_game_variables
+			mono_launcher_environment "$application"
+
+			# Generate the game prefix
 			launcher_print_persistent_paths
 			launcher_prefix_symlinks_functions
 			launcher_prefix_symlinks_build
+
 			mono_launcher_run "$application"
 			launcher_prefix_symlinks_cleanup
 			launcher_exit
 		;;
 		('none')
 			launcher_headers
-			mono_launcher_application_variables "$application"
-			launcher_game_variables
+			mono_launcher_environment "$application"
 			mono_launcher_run "$application"
 			launcher_exit
 		;;
@@ -32,87 +33,90 @@ mono_launcher() {
 	esac
 }
 
-# Mono - Print application-specific variables
-# USAGE: mono_launcher_application_variables $application
-mono_launcher_application_variables() {
+# Mono launcher - Set the environment
+# USAGE: mono_launcher_environment $application
+mono_launcher_environment() {
 	local application
 	application="$1"
 
-	local application_exe application_libs application_options application_mono_options
+	local game_id path_game application_exe application_options
+	game_id=$(game_id)
+	path_game=$(path_game_data)
 	application_exe=$(application_exe_escaped "$application")
 	application_libs=$(application_libs "$application")
 	application_options=$(application_options "$application")
-	application_mono_options=$(application_mono_options "$application")
 
 	cat <<- EOF
-	# Set application-specific values
+	# Set the environment
+
+	GAME_ID='$game_id'
+	PATH_GAME='$path_game'
 	APP_EXE='$application_exe'
 	APP_LIBS='$application_libs'
 	APP_OPTIONS="$application_options"
-	MONO_OPTIONS='$application_mono_options'
+
 	EOF
 }
 
-# Mono - Print the actual call to mono
+# Mono launcher - Run Mono
 # USAGE: mono_launcher_run $application
 mono_launcher_run() {
 	local application
 	application="$1"
 
-	local exec_path application_prerun application_postrun libraries_paths launcher_tweaks
-	exec_path=$(mono_launcher_exec_path "$application")
-	application_prerun=$(application_prerun "$application")
-	application_postrun=$(application_postrun "$application")
-	libraries_paths=$(native_launcher_libraries)
-	launcher_tweaks=$(mono_launcher_tweaks)
-
+	local prefix_type execution_path
+	prefix_type=$(application_prefix_type "$application")
+	case "$prefix_type" in
+		('symlinks')
+			execution_path='$PATH_PREFIX'
+		;;
+		('none')
+			execution_path='$PATH_GAME'
+		;;
+	esac
 	cat <<- EOF
 	# Run the game
-	cd "$exec_path"
-	$application_prerun
-	$libraries_paths
-	$launcher_tweaks
+
+	cd "$execution_path"
+
+	EOF
+
+	# Set loading paths for libraries
+	native_launcher_libraries
+
+	application_prerun "$application"
+
+	# Apply common workarounds for Mono games
+	mono_launcher_tweaks
+
+	cat <<- 'EOF'
 	## Do not exit on application failure,
 	## to ensure post-run commands are run.
 	set +o errexit
-	mono \$MONO_OPTIONS "\$APP_EXE" \$APP_OPTIONS "\$@"
-	game_exit_status=\$?
+
+	## Silence ShellCheck false-positive
+	## Double quote to prevent globbing and word splitting.
+	# shellcheck disable=SC2086
+	mono "$APP_EXE" $APP_OPTIONS "$@"
+
+	game_exit_status=$?
 	set -o errexit
-	$application_postrun
+
 	EOF
+
+	application_postrun "$application"
 }
 
-# Mono - Print common workarounds
+# Mono launcher - Common workarounds
 # USAGE: mono_launcher_tweaks
 mono_launcher_tweaks() {
 	cat <<- 'EOF'
 	## Work around terminfo Mono bug,
 	## cf. https://github.com/mono/mono/issues/6752
 	export TERM="${TERM%-256color}"
+
 	## Work around Mono unpredictable behaviour with non-US locales
 	export LANG=C
+
 	EOF
-}
-
-# Mono - Print the path from where the game binary is called.
-# USAGE: mono_launcher_exec_path $application
-mono_launcher_exec_path() {
-	local application
-	application="$1"
-
-	local prefix_type
-	prefix_type=$(application_prefix_type "$application")
-	case "$prefix_type" in
-		('symlinks')
-			printf '$PATH_PREFIX'
-			return 0
-		;;
-		('none')
-			printf '$PATH_GAME'
-			return 0
-		;;
-		(*)
-			return 1
-		;;
-	esac
 }
