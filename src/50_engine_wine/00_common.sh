0e5b2ac06fd2f83876edcd8611777ab809b56eb5
Antoine Le Gonidec <vv221@dotslashplay.it>
2023-07-12 16:58:55 +0200

    Unreal Engine 4 - Use a default path for user data diversion from the WINE prefix

--- src/50_engine_wine/00_common.sh
+++ src/50_engine_wine/00_common.sh
@@ -3,7 +3,20 @@
 # RETURN: A list of path to directories,
 #         separated by line breaks.
 wine_persistent_directories() {
-	context_value 'WINE_PERSISTENT_DIRECTORIES'
+	local persistent_directories
+	persistent_directories=$(context_value 'WINE_PERSISTENT_DIRECTORIES')
+
+	# Fall back on default values based on the game engine
+	if [ -z "$persistent_directories" ]; then
+		## Unreal Engine 4
+		local unrealengine4_name
+		unrealengine4_name=$(unrealengine4_name)
+		if [ -n "$unrealengine4_name" ]; then
+			persistent_directories=$(unrealengine4_wine_persistent_directories_default)
+		fi
+	fi
+
+	printf '%s' "$persistent_directories"
 }
 
 # WINE - Print the list of winetricks verb that should be applied during the WINE prefix initialization
--- src/55_engine-variant_unrealengine4/50_wine.sh
+++ src/55_engine-variant_unrealengine4/50_wine.sh
@@ -4,3 +4,14 @@ unrealengine4_wine_renderer_name_default() {
 	printf '%s' 'dxvk'
 }
 
+# Unreal Engine 4 - Print the paths relative to the WINE prefix that should be diverted to persistent storage
+# USAGE: unrealengine4_wine_persistent_directories_default
+# RETURN: A list of path to directories,
+#         separated by line breaks.
+unrealengine4_wine_persistent_directories_default() {
+	local unrealengine4_name
+	unrealengine4_name=$(unrealengine4_name)
+
+	printf 'users/${USER}/AppData/Local/%s/Saved' "$unrealengine4_name"
+}
+
