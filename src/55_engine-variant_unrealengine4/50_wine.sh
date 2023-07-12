b852db6957c9684d09811d39287a64d78edb3822
Antoine Le Gonidec <vv221@dotslashplay.it>
2023-07-12 16:51:44 +0200

    Unreal Engine 4 - Use DXVK as the default WINE Direct3D renderer

---
+++ src/55_engine-variant_unrealengine4/50_wine.sh
@@ -0,0 +1,6 @@
+# Unreal Engine 4 - Print the name of the renderer to use for Direct3D
+# USAGE: unrealengine4_wine_renderer_name_default
+unrealengine4_wine_renderer_name_default() {
+	printf '%s' 'dxvk'
+}
+
