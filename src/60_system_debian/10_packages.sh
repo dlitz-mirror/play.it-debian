787ea6202f3bc7932af80ccd2f5155f2966945c0
Antoine Le Gonidec <vv221@dotslashplay.it>
2023-07-21 20:06:32 +0200

    Prevent incompatibility between old .deb format 0.939000 and xz compression

--- src/60_system_debian/10_packages.sh
+++ src/60_system_debian/10_packages.sh
@@ -132,6 +132,15 @@ debian_package_build_single() {
 		return 0
 	fi
 
+	# Use old .deb format if the package is going over the size limit for the modern format
+	local package_size
+	package_size=$(debian_package_size "$package")
+	if [ "$package_size" -gt 9700000 ]; then
+		warning_debian_size_limit "$package"
+		export PLAYIT_DEBIAN_OLD_DEB_FORMAT=1
+		dpkg_options="${dpkg_options} --deb-format=0.939000"
+	fi
+
 	# Set compression setting
 	local option_compression dpkg_options
 	option_compression=$(option_value 'compression')
@@ -144,7 +153,18 @@ debian_package_build_single() {
 			dpkg_options="${dpkg_options} -Zgzip"
 		;;
 		('size')
-			dpkg_options="${dpkg_options} -Zxz"
+			if [ "${PLAYIT_DEBIAN_OLD_DEB_FORMAT:-0}" -eq 1 ]; then
+				## Old .deb format 0.939000 is not compatible with xz compression.
+				dpkg_options="${dpkg_options} -Zgzip"
+			else
+				dpkg_options="${dpkg_options} -Zxz"
+			fi
+		;;
+		('auto')
+			if [ "${PLAYIT_DEBIAN_OLD_DEB_FORMAT:-0}" -eq 1 ]; then
+				## Old .deb format 0.939000 is not compatible with xz compression.
+				dpkg_options="${dpkg_options} -Zgzip"
+			fi
 		;;
 		('gzip'|'xz')
 			if ! version_is_at_least '2.23' "$target_version"; then
@@ -152,15 +172,6 @@ debian_package_build_single() {
 			fi
 		;;
 	esac
-
-	# Use old .deb format if the package is going over the size limit for the modern format
-	local package_size
-	package_size=$(debian_package_size "$package")
-	if [ "$package_size" -gt 9700000 ]; then
-		warning_debian_size_limit "$package"
-		export PLAYIT_DEBIAN_OLD_DEB_FORMAT=1
-		dpkg_options="${dpkg_options} --deb-format=0.939000"
-	fi
 
 	# Run the actual package generation, using dpkg-deb
 	local package_generation_return_code
