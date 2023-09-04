41c07ae1d370793271376a8214a83d83949d5f45
Antoine Le Gonidec <vv221@dotslashplay.it>
2023-09-04 19:06:10 +0200

    Gentoo - Fix the function call used to fill the DESCRIPTION ebuild field

--- src/60_system_gentoo/10_packages_gentoo-variant.sh
+++ src/60_system_gentoo/10_packages_gentoo-variant.sh
@@ -50,7 +50,7 @@ gentoo_package_metadata_single() {
 
 	local ebuild_path package_architecture ebuild_keywords ebuild_description ebuild_rdepend
 	ebuild_path=$(gentoo_ebuild_path "$package")
-	ebuild_description=$(ebuild_description "$package")
+	ebuild_description=$(package_description "$package")
 	ebuild_rdepend=$(package_gentoo_ebuild_rdepend "$pkg")
 	package_architecture=$(package_architecture "$package")
 	case "$package_architecture" in
